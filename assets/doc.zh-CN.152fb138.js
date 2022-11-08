dd2e2b8579a533ab57d01bc7c82543597c733247
xxxsf <allan91coder@gmail.com>
2022-11-08 01:39:27 +0000

    Deploying to main from @ hellof2e/quark-design-docs@a8cd8f0f8375f07b73ff85bf04b2e75ed5b8706c 🚀

---
+++ assets/doc.zh-CN.152fb138.js
@@ -0,0 +1,47 @@
+import{d as a,c as t,a as u,o as n}from"./vue.72a56600.js";const l={class:"markdown-body"},p=u(`<h1>Popupextra</h1><h3>\u4ECB\u7ECD</h3><p>\u6D6E\u5C42</p><h3>\u5B89\u88C5</h3><pre><code class="language-tsx"><span class="hljs-keyword">import</span> <span class="hljs-string">&#39;quarkd/lib/popupextra&#39;</span>;
+</code></pre><h3>\u57FA\u672C\u4F7F\u7528</h3><pre><code class="language-html"><span class="hljs-tag">&lt;<span class="hljs-name">quark-popupextra</span>
+  <span class="hljs-attr">title</span>=<span class="hljs-string">&quot;\u5927\u6807\u9898\u6587\u6848&quot;</span>
+  <span class="hljs-attr">subtitle</span>=<span class="hljs-string">&quot;\u526F\u6807\u9898\u6587\u6848&quot;</span>
+  <span class="hljs-attr">:open</span>=<span class="hljs-string">&quot;open&quot;</span>
+  @<span class="hljs-attr">closed</span>=<span class="hljs-string">&quot;open = false&quot;</span>
+&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;popup-body&quot;</span>&gt;</span>
+    <span class="hljs-tag">&lt;<span class="hljs-name">h4</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;&quot;</span>&gt;</span>1.\u6B63\u6B63\u6B63\u6B63\u6B63\u6B63\u6B63\u6B63\u6B63\u6B63\u6B63<span class="hljs-tag">&lt;/<span class="hljs-name">h4</span>&gt;</span>
+    <span class="hljs-tag">&lt;<span class="hljs-name">p</span>&gt;</span>\u5B6B\u5B50\u66F0\uFF1A\u5175\u8005\uFF0C\u570B\u4E4B\u5927\u4E8B\uFF0C\u6B7B\u751F\u4E4B\u5730\uFF0C\u5B58\u4EA1\u4E4B\u9053\u3002<span class="hljs-tag">&lt;/<span class="hljs-name">p</span>&gt;</span>
+  <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+<span class="hljs-tag">&lt;/<span class="hljs-name">quark-popupextra</span>&gt;</span>
+</code></pre><pre><code class="language-js">&lt;script&gt;
+<span class="hljs-keyword">import</span> <span class="hljs-string">&#39;quarkd/lib/popupextra&#39;</span>;
+
+<span class="hljs-keyword">export</span> <span class="hljs-keyword">default</span> {
+  <span class="hljs-function"><span class="hljs-title">data</span>(<span class="hljs-params"></span>)</span> {
+    <span class="hljs-keyword">return</span> {
+     <span class="hljs-attr">open</span>: <span class="hljs-literal">false</span>,
+    }
+   },
+  <span class="hljs-attr">methods</span>: {
+   <span class="hljs-function"><span class="hljs-title">showPopup</span>(<span class="hljs-params"></span>)</span> {
+     <span class="hljs-built_in">this</span>.open = <span class="hljs-literal">true</span>;
+   },
+ }
+}
+&lt;/script&gt;
+</code></pre><h3>\u81EA\u5B9A\u4E49\u6807\u9898</h3><p>\u901A\u8FC7\u8BBE\u7F6E <code class="">slot=&#39;title&#39;</code> \u53EF\u4EE5\u8986\u76D6\u5C5E\u6027\u4E2D\u7684 <code class="">title</code>\uFF0C\u4ECE\u800C\u5B9E\u73B0\u81EA\u5B9A\u4E49 <code class="">title</code>\u3002</p><pre><code class="language-html"><span class="hljs-tag">&lt;<span class="hljs-name">quark-popupextra</span>
+  <span class="hljs-attr">subtitle</span>=<span class="hljs-string">&quot;\u526F\u6807\u9898\u6587\u6848&quot;</span>
+  <span class="hljs-attr">:open</span>=<span class="hljs-string">&quot;open&quot;</span>
+  @<span class="hljs-attr">closed</span>=<span class="hljs-string">&quot;open = false&quot;</span>&gt;</span>
+
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">slot</span>=<span class="hljs-string">&quot;title&quot;</span>&gt;</span>
+    <span class="hljs-tag">&lt;<span class="hljs-name">span</span> <span class="hljs-attr">style</span>=<span class="hljs-string">&quot;color: red&quot;</span>&gt;</span>\u81EA\u5B9A\u4E49<span class="hljs-tag">&lt;/<span class="hljs-name">span</span>&gt;</span>\u5927\u6807\u9898\u6587\u6848
+  <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;popup-body&quot;</span>&gt;</span>
+    <span class="hljs-tag">&lt;<span class="hljs-name">h4</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;&quot;</span>&gt;</span>1.\u6B63\u6B63\u6B63\u6B63\u6B63\u6B63\u6B63\u6B63\u6B63\u6B63\u6B63<span class="hljs-tag">&lt;/<span class="hljs-name">h4</span>&gt;</span>
+    <span class="hljs-tag">&lt;<span class="hljs-name">p</span>&gt;</span>\u5B6B\u5B50\u66F0\uFF1A\u5175\u8005\uFF0C\u570B\u4E4B\u5927\u4E8B\uFF0C\u6B7B\u751F\u4E4B\u5730\uFF0C\u5B58\u4EA1\u4E4B\u9053\uFF0C\u4E0D\u53EF\u4E0D\u5BDF\u4E5F\u3002<span class="hljs-tag">&lt;/<span class="hljs-name">p</span>&gt;</span>
+  <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+<span class="hljs-tag">&lt;/<span class="hljs-name">quark-popupextra</span>&gt;</span>
+</code></pre><h3>\u5173\u95ED\u6D6E\u5C42</h3><p>\u8BBE\u7F6E <code class="">closed</code> \u65B9\u6CD5\uFF0C\u53EF\u4EE5\u5173\u95ED\u6D6E\u5C42</p><pre><code class="language-html"><span class="hljs-tag">&lt;<span class="hljs-name">quark-popupextra</span> <span class="hljs-attr">:open</span>=<span class="hljs-string">&quot;open&quot;</span> @<span class="hljs-attr">closed</span>=<span class="hljs-string">&quot;handleClose()&quot;</span> /&gt;</span>
+
+// handleClose
+this.open = false
+</code></pre><h2>API</h2><h3>Props</h3><table><thead><tr><th>\u53C2\u6570</th><th>\u8BF4\u660E</th><th>\u7C7B\u578B</th><th>\u9ED8\u8BA4\u503C</th></tr></thead><tbody><tr><td>open</td><td>\u5F39\u7A97\u72B6\u6001</td><td><code class="">boolean</code></td><td><code class="">require</code></td></tr><tr><td>title</td><td>\u4E3B\u6807\u9898</td><td><code class="">string</code></td><td></td></tr><tr><td>subtitle</td><td>\u4E3B\u6807\u9898</td><td><code class="">string</code></td><td></td></tr><tr><td>hideclose</td><td>\u662F\u5426\u9690\u85CF\u53F3\u4E0A\u89D2\u5173\u95ED\u6309\u94AE</td><td><code class="">boolean </code></td><td><code class="">false</code></td></tr><tr><td>round</td><td>\u662F\u5426\u5706\u89D2</td><td><code class="">boolean </code></td><td><code class="">true</code></td></tr><tr><td>safearea</td><td>\u662F\u5426\u5F00\u542F\u5E95\u90E8\u5B89\u5168\u533A\u57DF\u9002\u914D</td><td><code class="">boolean</code></td><td><code class="">false</code></td></tr><tr><td>zindex</td><td>popup\u5C42\u7EA7\u8BBE\u7F6E</td><td><code class="">number\u3001string</code></td><td>-</td></tr></tbody></table><h3>Event</h3><table><thead><tr><th>\u540D\u79F0</th><th>\u8BF4\u660E</th><th>\u7C7B\u578B</th></tr></thead><tbody><tr><td>closed</td><td>\u7EC4\u4EF6\u5173\u95ED\u56DE\u8C03</td><td><code class="">\uFF08\uFF09=&gt; void</code></td></tr></tbody></table><h2>\u6837\u5F0F\u53D8\u91CF</h2><p>\u7EC4\u4EF6\u63D0\u4F9B\u4E86\u4EE5\u4E0B<a href="https://developer.mozilla.org/zh-CN/docs/Web/CSS/Using_CSS_custom_properties">CSS\u53D8\u91CF</a>\uFF0C\u53EF\u7528\u4E8E\u81EA\u5B9A\u4E49\u6837\u5F0F\uFF0C\u4F7F\u7528\u65B9\u6CD5\u8BF7\u53C2\u8003<a href="#/zh-CN/guide/theme">\u4E3B\u9898\u5B9A\u5236</a>\u3002</p><table><thead><tr><th>\u540D\u79F0</th><th>\u8BF4\u660E</th><th>\u9ED8\u8BA4\u503C</th></tr></thead><tbody><tr><td><code class="">--popup-extra-title-color</code></td><td>\u4E3B\u6807\u9898\u989C\u8272</td><td><code class="">#242729</code></td></tr><tr><td><code class="">--popup-extra-subtitle-color</code></td><td>\u526F\u6807\u9898\u989C\u8272</td><td><code class="">#879099</code></td></tr><tr><td><code class="">--popup-extra-mask-color</code></td><td>\u8499\u5C42\u80CC\u666F</td><td><code class="">rgba(0, 0, 0, 0.7)</code></td></tr><tr><td><code class="">--popup-extra-z-index</code></td><td>\u7EC4\u4EF6\u5C42\u7EA7</td><td><code class="">999</code></td></tr><tr><td><code class="">--popup-extra-max-height</code></td><td>popup\u5185\u5BB9\u533A\u57DF\u6700\u5927\u9AD8\u5EA6</td><td><code class="">90%</code></td></tr><tr><td><code class="">--popup-extra-min-height</code></td><td>popup\u5185\u5BB9\u533A\u57DF\u6700\u5C0F\u9AD8\u5EA6</td><td>-</td></tr><tr><td><code class="">--popup-extra-border-radius</code></td><td>popup\u5706\u89D2\u5927\u5C0F</td><td><code class="">16px</code></td></tr><tr><td><code class="">--popup-extra-title-font-family</code></td><td>popup \u4E3B\u6807\u9898 font-family</td><td><code class="">PingFangSC-Medium, PingFang SC, sans-serif</code></td></tr><tr><td><code class="">--popup-extra-subtitle-font-family</code></td><td>popup \u526F\u6807\u9898 font-family</td><td><code class="">PingFangSC-Regular, PingFang SC, sans-serif</code></td></tr></tbody></table>`,22),e=[p],h={},j="",g=a({__name:"doc.zh-CN",setup(d,{expose:s}){return s({frontmatter:{},excerpt:void 0}),(c,o)=>(n(),t("div",l,e))}});export{g as default,j as excerpt,h as frontmatter};
---
+++ assets/internationalization.zh-CN.ce54dc6c.js
@@ -0,0 +1,37 @@
+import{d as u,c as a,a as t,o as n}from"./vue.72a56600.js";const l={class:"markdown-body"},p=t(`<h1>\u56FD\u9645\u5316</h1><h2>\u4ECB\u7ECD</h2><p>Quark \u91C7\u7528\u4E2D\u6587\u4F5C\u4E3A\u9ED8\u8BA4\u8BED\u8A00\uFF0C\u540C\u65F6\u652F\u6301\u591A\u8BED\u8A00\u5207\u6362\uFF0C\u8BF7\u6309\u7167\u4E0B\u65B9\u6559\u7A0B\u8FDB\u884C\u56FD\u9645\u5316\u8BBE\u7F6E\u3002</p><h2>\u591A\u8BED\u8A00\u5207\u6362</h2><p>Quark \u901A\u8FC7 Locale \u7EC4\u4EF6\u5B9E\u73B0\u591A\u8BED\u8A00\u652F\u6301\uFF0C\u4F7F\u7528 Locale.use \u65B9\u6CD5\u53EF\u4EE5\u5207\u6362\u5F53\u524D\u4F7F\u7528\u7684\u8BED\u8A00\u3002</p><pre><code class="language-ts"><span class="hljs-keyword">import</span> Locale <span class="hljs-keyword">from</span> <span class="hljs-string">&quot;quarkd/lib/locale&quot;</span>;
+<span class="hljs-comment">// \u5F15\u5165\u82F1\u6587\u8BED\u8A00\u5305</span>
+<span class="hljs-keyword">import</span> enUS <span class="hljs-keyword">from</span> <span class="hljs-string">&quot;quarkd/lib/locale/lang/en-US&quot;</span>;
+
+Locale.use(enUS);
+</code></pre><h2>\u8986\u76D6\u8BED\u8A00\u5305</h2><p>\u901A\u8FC7 Locale.add \u65B9\u6CD5\u53EF\u4EE5\u5B9E\u73B0\u6587\u6848\u7684\u4FEE\u6539\uFF0C\u793A\u4F8B\u5982\u4E0B\uFF1A</p><pre><code class="language-ts"><span class="hljs-keyword">import</span> Locale <span class="hljs-keyword">from</span> <span class="hljs-string">&quot;quarkd/lib/locale&quot;</span>;
+
+Locale.add({
+  <span class="hljs-attr">confirm</span>: <span class="hljs-string">&quot;Custom confirm&quot;</span>,
+  <span class="hljs-attr">pullRefresh</span>: {
+    <span class="hljs-attr">pulling</span>: <span class="hljs-string">&quot;Custom pull to refresh...&quot;</span>,
+    <span class="hljs-attr">loosing</span>: <span class="hljs-string">&quot;Custom loose to refresh...&quot;</span>,
+  },
+});
+</code></pre><h2>\u76EE\u524D\u652F\u6301\u7684\u8BED\u8A00</h2><table><thead><tr><th>\u8BED\u8A00</th><th>\u6587\u4EF6\u540D</th><th>\u7248\u672C</th></tr></thead><tbody><tr><td>\u82F1\u8BED</td><td>en-US</td><td><code class="">v1.0.0</code></td></tr><tr><td>\u7B80\u4F53\u4E2D\u6587</td><td>zh-CN</td><td><code class="">v1.0.0</code></td></tr><tr><td>\u7E41\u9AD4\u4E2D\u6587</td><td>zh-TW</td><td>\u7B49\u5F85 PR</td></tr><tr><td>\u5370\u5EA6\u5C3C\u897F\u4E9A\u8BED</td><td>id-ID</td><td>\u7B49\u5F85 PR</td></tr><tr><td>\u6CF0\u8BED</td><td>th-TH</td><td>\u7B49\u5F85 PR</td></tr></tbody></table><h2>\u5185\u7F6E\u7684\u53D8\u91CF</h2><pre><code class="language-ts"><span class="hljs-comment">// \u4E2D\u6587\u793A\u4F8B</span>
+<span class="hljs-keyword">export</span> <span class="hljs-keyword">default</span> {
+  <span class="hljs-attr">save</span>: <span class="hljs-string">&quot;\u4FDD\u5B58&quot;</span>,
+  <span class="hljs-attr">confirm</span>: <span class="hljs-string">&quot;\u786E\u8BA4&quot;</span>,
+  <span class="hljs-attr">cancel</span>: <span class="hljs-string">&quot;\u53D6\u6D88&quot;</span>,
+  <span class="hljs-attr">delete</span>: <span class="hljs-string">&quot;\u5220\u9664&quot;</span>,
+  <span class="hljs-attr">loading</span>: <span class="hljs-string">&quot;\u52A0\u8F7D\u4E2D...&quot;</span>,
+  <span class="hljs-attr">placehold</span>: <span class="hljs-string">&quot;\u8BF7\u8F93\u5165\u5185\u5BB9&quot;</span>,
+  <span class="hljs-attr">image</span>: {
+    <span class="hljs-attr">loadError</span>: <span class="hljs-string">&quot;\u52A0\u8F7D\u5931\u8D25&quot;</span>,
+  },
+  <span class="hljs-attr">pullRefresh</span>: {
+    <span class="hljs-attr">pulling</span>: <span class="hljs-string">&quot;\u4E0B\u62C9\u5373\u53EF\u5237\u65B0...&quot;</span>,
+    <span class="hljs-attr">loosing</span>: <span class="hljs-string">&quot;\u91CA\u653E\u5373\u53EF\u5237\u65B0...&quot;</span>,
+  },
+  <span class="hljs-attr">search</span>: {
+    <span class="hljs-attr">placeholder</span>: <span class="hljs-string">&quot;\u8BF7\u8F93\u5165\u641C\u7D22\u5173\u952E\u8BCD&quot;</span>,
+  },
+  <span class="hljs-attr">actionSheet</span>: {
+    <span class="hljs-attr">shareTitle</span>: <span class="hljs-string">&quot;\u5206\u4EAB\u5230&quot;</span>,
+  },
+};
+</code></pre><h2>\u4E1A\u52A1\u4EE3\u7801\u5982\u4F55\u5B9E\u73B0\u56FD\u9645\u5316\uFF1F</h2><p>\u53EF\u4EE5\u4F7F\u7528 <code class="">i18n</code> \u6765\u5B9E\u73B0\u3002</p>`,15),o=[p],d={},F="",E=u({__name:"internationalization.zh-CN",setup(r,{expose:s}){return s({frontmatter:{},excerpt:void 0}),(c,e)=>(n(),a("div",l,o))}});export{E as default,F as excerpt,d as frontmatter};
