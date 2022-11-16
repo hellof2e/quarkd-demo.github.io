a8cd87a56c8b11ca4b99292fe0c5e7208ec37d01
sanqi-med <huiqin131460@163.com>
2022-11-16 02:28:46 +0000

    Deploying to main from @ hellof2e/quark-design@67390225419d850751b8f7f99198bf084045d2bc 🚀

---
+++ demo/assets/demo.546398eb.js
@@ -0,0 +1,270 @@
+import { c as createComponent } from "./component.31b4b8c6.js";
+import { _ as _export_sfc, r as ref, o as onBeforeMount, u as useTranslate, f as onMounted, c as createElementBlock, a as createBaseVNode, t as toDisplayString, T as Toast, b as openBlock } from "./mobile.a201d406.js";
+var demo_scss_vue_type_style_index_0_src_lang = "";
+const { createDemo, translate } = createComponent("field");
+const _sfc_main = createDemo({
+  setup() {
+    const value = ref(1);
+    const disabled = ref(true);
+    const css = ref({
+      labelWidth: "100px",
+      labelColor: "#ccc",
+      labelSize: "16px",
+      labelMarginRight: "10px",
+      inputColor: "#999",
+      inputSize: "12px",
+      placeholderColor: "red"
+    });
+    onBeforeMount(() => {
+      useTranslate({
+        "zh-CN": {
+          title: {
+            basic: "\u57FA\u7840\u7528\u6CD5",
+            head: "\u81EA\u5B9A\u4E49\u6807\u9898/\u65E0\u6807\u9898",
+            disabled: "\u7981\u7528\u4E0E\u53EA\u8BFB",
+            css: "css \u5C5E\u6027",
+            event: "\u4E8B\u4EF6",
+            required: "\u8BBE\u7F6E\u5FC5\u586B\u5B57\u6BB5"
+          },
+          basic: {
+            placeholder: "\u8BF7\u8F93\u5165\u6587\u672C",
+            label1: "\u6587\u672C",
+            label2: "\u5BC6\u7801",
+            label3: "\u6570\u5B57",
+            label4: "\u6700\u591A5\u4F4D\u6570",
+            value: "\u4E00\u4E8C\u4E09\u56DB\u4E94"
+          },
+          head: {
+            value: "\u81EA\u5B9A\u4E49\u6807\u9898",
+            main: "\u4E3B\u6807\u9898",
+            subtitle: "\u8FD9\u662F\u4E00\u884C\u526F\u6807\u9898"
+          },
+          nohead: {
+            placeholder: "\u7981\u7528label",
+            value: "\u65E0\u6807\u9898"
+          },
+          disabled: {
+            value: "\u6211\u7981\u7528\u4E86",
+            label: "\u7981\u7528"
+          },
+          readonly: {
+            value: "\u6211\u662F\u53EA\u8BFB\u7684",
+            label: "\u53EA\u8BFB"
+          },
+          requiredText: {
+            placeholder: "\u8BF7\u8F93\u5165\u6587\u672C",
+            label: "\u6587\u672C",
+            error: "\u6587\u672C\u5185\u5BB9\u4E0D\u80FD\u4E3A\u7A7A"
+          },
+          requiredPhone: {
+            placeholder: "\u8BF7\u8F93\u5165\u624B\u673A\u53F7",
+            label: "\u624B\u673A\u53F7",
+            error: "\u8BF7\u8F93\u5165\u6B63\u786E\u7684\u624B\u673A\u53F7"
+          }
+        },
+        "en-US": {
+          title: {
+            basic: "Basic Usage",
+            head: "Custom Title",
+            disabled: "Disabled & Readonly",
+            css: "CSS Style",
+            event: "Event",
+            required: "Requied Content Setting"
+          },
+          basic: {
+            placeholder: "Please enter text",
+            label1: "Text",
+            label2: "Password",
+            label3: "Number",
+            label4: "Up to 5 digits",
+            value: "One Two Tree Four Five"
+          },
+          head: {
+            value: "Custom Title",
+            main: "Main Title",
+            subtitle: "Subtitle"
+          },
+          nohead: {
+            placeholder: "Disable Label",
+            value: "No Title"
+          },
+          disabled: {
+            value: "Disabled",
+            label: "Disabled"
+          },
+          readonly: {
+            value: "Readonly",
+            label: "Readonly"
+          },
+          requiredText: {
+            placeholder: "Please enter text",
+            label: "Text",
+            error: "Text content cannot be empty"
+          },
+          requiredPhone: {
+            placeholder: "Please enter phone number",
+            label: "Phone number",
+            error: "Please enter the correct number"
+          }
+        }
+      });
+    });
+    onMounted(() => {
+      const field = document.getElementById("custom-rule");
+      field.setRules([
+        {
+          validator: validatorPhone,
+          message: `${translate("requiredPhone.error")}`
+        }
+      ]);
+    });
+    const validatorPhone = (value2) => {
+      if (!/^1[3456789]\d{9}$/.test(value2)) {
+        return false;
+      }
+      return true;
+    };
+    const change = (e) => {
+      value.data = e.detail.value;
+      Toast.text(e.detail.value);
+    };
+    const blur = () => {
+      Toast.text("blur");
+    };
+    const focus = () => {
+      Toast.text("focus");
+    };
+    return {
+      value,
+      disabled,
+      css,
+      change,
+      blur,
+      focus,
+      translate
+    };
+  }
+});
+const _hoisted_1 = { class: "demo field-demo" };
+const _hoisted_2 = { class: "demo-inputs" };
+const _hoisted_3 = { class: "type" };
+const _hoisted_4 = ["placeholder", "label"];
+const _hoisted_5 = /* @__PURE__ */ createBaseVNode("div", { class: "line" }, null, -1);
+const _hoisted_6 = ["label"];
+const _hoisted_7 = /* @__PURE__ */ createBaseVNode("div", { class: "line" }, null, -1);
+const _hoisted_8 = ["label"];
+const _hoisted_9 = /* @__PURE__ */ createBaseVNode("div", { class: "line" }, null, -1);
+const _hoisted_10 = ["value", "label"];
+const _hoisted_11 = { class: "type" };
+const _hoisted_12 = ["value"];
+const _hoisted_13 = {
+  slot: "label",
+  class: "label"
+};
+const _hoisted_14 = /* @__PURE__ */ createBaseVNode("div", { class: "line" }, null, -1);
+const _hoisted_15 = ["placeholder", "value"];
+const _hoisted_16 = /* @__PURE__ */ createBaseVNode("div", { class: "line" }, null, -1);
+const _hoisted_17 = { class: "type" };
+const _hoisted_18 = ["value", "label"];
+const _hoisted_19 = /* @__PURE__ */ createBaseVNode("div", { class: "line" }, null, -1);
+const _hoisted_20 = ["value", "label"];
+const _hoisted_21 = { class: "type" };
+const _hoisted_22 = ["value"];
+const _hoisted_23 = ["label", "value"];
+const _hoisted_24 = ["placeholder", "label", "errormsg"];
+const _hoisted_25 = ["placeholder", "label"];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", _hoisted_1, [
+    createBaseVNode("div", _hoisted_2, [
+      createBaseVNode("h2", null, toDisplayString(_ctx.translate("title.basic")), 1),
+      createBaseVNode("div", _hoisted_3, [
+        createBaseVNode("quark-field", {
+          placeholder: _ctx.translate("basic.placeholder"),
+          label: _ctx.translate("basic.label1")
+        }, null, 8, _hoisted_4),
+        _hoisted_5,
+        createBaseVNode("quark-field", {
+          type: "password",
+          value: "123456",
+          label: _ctx.translate("basic.label2")
+        }, null, 8, _hoisted_6),
+        _hoisted_7,
+        createBaseVNode("quark-field", {
+          type: "number",
+          value: "12345678901",
+          max: "11",
+          label: _ctx.translate("basic.label3")
+        }, null, 8, _hoisted_8),
+        _hoisted_9,
+        createBaseVNode("quark-field", {
+          value: _ctx.translate("basic.value"),
+          maxlength: "5",
+          label: _ctx.translate("basic.label4")
+        }, null, 8, _hoisted_10)
+      ]),
+      createBaseVNode("h2", null, toDisplayString(_ctx.translate("title.head")), 1),
+      createBaseVNode("div", _hoisted_11, [
+        createBaseVNode("quark-field", {
+          value: _ctx.translate("head.value")
+        }, [
+          createBaseVNode("div", _hoisted_13, [
+            createBaseVNode("p", null, toDisplayString(_ctx.translate("head.main")), 1),
+            createBaseVNode("span", null, toDisplayString(_ctx.translate("head.subtitle")), 1)
+          ])
+        ], 8, _hoisted_12),
+        _hoisted_14,
+        createBaseVNode("quark-field", {
+          placeholder: _ctx.translate("nohead.placeholder"),
+          value: _ctx.translate("nohead.value"),
+          class: "oneLine"
+        }, null, 8, _hoisted_15),
+        _hoisted_16
+      ]),
+      createBaseVNode("h2", null, toDisplayString(_ctx.translate("title.disabled")), 1),
+      createBaseVNode("div", _hoisted_17, [
+        createBaseVNode("quark-field", {
+          value: _ctx.translate("disabled.value"),
+          label: _ctx.translate("disabled.label"),
+          disabled: ""
+        }, null, 8, _hoisted_18),
+        _hoisted_19,
+        createBaseVNode("quark-field", {
+          value: _ctx.translate("readonly.value"),
+          label: _ctx.translate("readonly.label"),
+          readonly: ""
+        }, null, 8, _hoisted_20)
+      ]),
+      createBaseVNode("h2", null, toDisplayString(_ctx.translate("title.css")), 1),
+      createBaseVNode("div", _hoisted_21, [
+        createBaseVNode("quark-field", {
+          class: "theme",
+          value: _ctx.translate("title.css"),
+          label: "css"
+        }, null, 8, _hoisted_22)
+      ]),
+      createBaseVNode("h2", null, toDisplayString(_ctx.translate("title.event")), 1),
+      createBaseVNode("quark-field", {
+        label: _ctx.translate("title.event"),
+        value: _ctx.value,
+        onChange: _cache[0] || (_cache[0] = (...args) => _ctx.change && _ctx.change(...args)),
+        onBlur: _cache[1] || (_cache[1] = (...args) => _ctx.blur && _ctx.blur(...args)),
+        onFocus: _cache[2] || (_cache[2] = (...args) => _ctx.focus && _ctx.focus(...args))
+      }, null, 40, _hoisted_23),
+      createBaseVNode("h2", null, toDisplayString(_ctx.translate("title.required")), 1),
+      createBaseVNode("quark-field", {
+        placeholder: _ctx.translate("requiredText.placeholder"),
+        label: _ctx.translate("requiredText.label"),
+        required: "",
+        errormsg: _ctx.translate("requiredText.error")
+      }, null, 8, _hoisted_24),
+      createBaseVNode("quark-field", {
+        placeholder: _ctx.translate("requiredPhone.placeholder"),
+        label: _ctx.translate("requiredPhone.label"),
+        id: "custom-rule",
+        required: ""
+      }, null, 8, _hoisted_25)
+    ])
+  ]);
+}
+var demo = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { demo as default };
---
+++ demo/assets/demo.859347f7.js
@@ -0,0 +1,233 @@
+import { c as createComponent } from "./component.31b4b8c6.js";
+import { _ as _export_sfc, r as ref, o as onBeforeMount, u as useTranslate, c as createElementBlock, a as createBaseVNode, t as toDisplayString, b as openBlock } from "./mobile.a201d406.js";
+import { o as onBeforeRouteLeave } from "./vue-router.1b3864b7.js";
+var demo_scss_vue_type_style_index_0_src_lang = "";
+const { createDemo, translate } = createComponent("dialog");
+const _sfc_main = createDemo({
+  setup() {
+    const open = ref(false);
+    const open2 = ref(false);
+    const open3 = ref(false);
+    const open4 = ref(false);
+    const open5 = ref(false);
+    const open6 = ref(false);
+    const open7 = ref(false);
+    const open8 = ref(false);
+    const open9 = ref(false);
+    onBeforeMount(() => {
+      useTranslate({
+        "zh-CN": {
+          basic: "\u57FA\u7840\u7528\u6CD5",
+          dialogTitle: "\u6807\u9898",
+          alert: "\u63D0\u793A\u5F39\u7A97",
+          alertWithoutContent: "\u63D0\u793A\u5F39\u7A97\uFF08\u6CA1\u6709\u5185\u5BB9\uFF09",
+          confirm: "\u786E\u8BA4\u5F39\u7A97",
+          btnvertical: "\u786E\u8BA4\u5F39\u7A97(\u6309\u94AE\u5782\u76F4\u6392\u5217)",
+          basicDialog: "\u57FA\u7840\u5F39\u7A97",
+          showCloseButton: "\u9690\u85CF\u5173\u95ED\u6309\u94AE",
+          alignLeft: "\u5185\u5BB9\u5C45\u5DE6\u5BF9\u9F50",
+          customFooter: "Footer \u81EA\u5B9A\u4E49",
+          noFooter: "Footer \u9690\u85CF",
+          title: "\u8FD9\u662F\u4E00\u53E5\u8BDD\u5C31\u80FD\u8BF4\u6E05\u695A\u7684\u786E\u8BA4\u5F39\u7A97\uFF0C\u6240\u4EE5\u53EA\u6709\u6807\u9898",
+          content: "\u751F\u547D\u8FDC\u4E0D\u6B62\u8FDE\u8F74\u8F6C\u548C\u5FD9\u5230\u6781\u9650\uFF0C\u4EBA\u7C7B\u7684\u4F53\u9A8C\u8FDC\u6BD4\u8FD9\u8FBD\u9614\u3001\u4E30\u5BCC\u5F97\u591A\u3002",
+          text: "\u4EE3\u7801\u662F\u5199\u51FA\u6765\u7ED9\u4EBA\u770B\u7684\uFF0C\u9644\u5E26\u80FD\u5728\u673A\u5668\u4E0A\u8FD0\u884C",
+          okText: "\u77E5\u9053\u4E86",
+          customTitle: "Title \u81EA\u5B9A\u4E49",
+          noTitle: "Title \u9690\u85CF"
+        },
+        "en-US": {
+          basic: "Basic use",
+          dialogTitle: "Title",
+          alert: "Alert",
+          alertWithoutContent: "Alert without content",
+          confirm: "Confirm dialog",
+          btnvertical: "Confirm dialog\uFF08vertical button\uFF09",
+          basicDialog: "Basic Dialog",
+          showCloseButton: "Hide Close Button",
+          alignLeft: "Align Content To The Left",
+          customFooter: "Custom Footer",
+          noFooter: "Not Show Footer ",
+          title: "This is a confirmation dialog that can be explained clearly in one sentence, so only the title",
+          content: "This is a content",
+          text: "This is custom body. The code is written for people to see, with the addition of running on the machine",
+          okText: "Got it",
+          noTitle: "Title hide",
+          customTitle: "Custom title"
+        }
+      });
+    });
+    onBeforeRouteLeave(() => {
+      const quarkDialogs = document.querySelectorAll("quark-dialog");
+      quarkDialogs.forEach((i) => i.open = false);
+    });
+    return {
+      open,
+      open2,
+      open3,
+      open4,
+      open5,
+      open6,
+      open7,
+      open8,
+      open9,
+      translate
+    };
+  }
+});
+const _hoisted_1 = { class: "demo demo-dialog" };
+const _hoisted_2 = ["title"];
+const _hoisted_3 = ["title"];
+const _hoisted_4 = ["title"];
+const _hoisted_5 = ["title"];
+const _hoisted_6 = ["open", "title", "content"];
+const _hoisted_7 = ["open", "title", "oktext"];
+const _hoisted_8 = ["open", "title", "content"];
+const _hoisted_9 = ["open", "title", "content"];
+const _hoisted_10 = /* @__PURE__ */ createBaseVNode("h2", null, "Title", -1);
+const _hoisted_11 = ["title"];
+const _hoisted_12 = ["title"];
+const _hoisted_13 = ["content", "open"];
+const _hoisted_14 = {
+  slot: "title",
+  style: { "color": "red", "font-size": "16px", "margin-bottom": "14px" }
+};
+const _hoisted_15 = ["open", "content"];
+const _hoisted_16 = /* @__PURE__ */ createBaseVNode("h2", null, "Footer", -1);
+const _hoisted_17 = ["title"];
+const _hoisted_18 = ["title"];
+const _hoisted_19 = ["title", "open"];
+const _hoisted_20 = {
+  slot: "footer",
+  class: "custom-footer"
+};
+const _hoisted_21 = ["open", "title", "content"];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", _hoisted_1, [
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("basic")), 1),
+    createBaseVNode("quark-cell-group", null, [
+      createBaseVNode("quark-cell", {
+        type: "primary",
+        isLink: "",
+        title: _ctx.translate("alert"),
+        onClick: _cache[0] || (_cache[0] = ($event) => _ctx.open = true)
+      }, null, 8, _hoisted_2),
+      createBaseVNode("quark-cell", {
+        type: "primary",
+        isLink: "",
+        title: _ctx.translate("alertWithoutContent"),
+        onClick: _cache[1] || (_cache[1] = ($event) => _ctx.open2 = true)
+      }, null, 8, _hoisted_3),
+      createBaseVNode("quark-cell", {
+        type: "primary",
+        isLink: "",
+        title: _ctx.translate("confirm"),
+        onClick: _cache[2] || (_cache[2] = ($event) => _ctx.open3 = true)
+      }, null, 8, _hoisted_4),
+      createBaseVNode("quark-cell", {
+        type: "primary",
+        isLink: "",
+        title: _ctx.translate("btnvertical"),
+        onClick: _cache[3] || (_cache[3] = ($event) => _ctx.open6 = true)
+      }, null, 8, _hoisted_5)
+    ]),
+    createBaseVNode("quark-dialog", {
+      type: "confirm",
+      open: _ctx.open,
+      title: _ctx.translate("dialogTitle"),
+      content: _ctx.translate("content"),
+      onConfirm: _cache[4] || (_cache[4] = ($event) => _ctx.open = false),
+      onClose: _cache[5] || (_cache[5] = ($event) => _ctx.open = false)
+    }, null, 40, _hoisted_6),
+    createBaseVNode("quark-dialog", {
+      type: "confirm",
+      open: _ctx.open2,
+      title: _ctx.translate("title"),
+      oktext: _ctx.translate("okText"),
+      onConfirm: _cache[6] || (_cache[6] = ($event) => _ctx.open2 = false),
+      onClose: _cache[7] || (_cache[7] = ($event) => _ctx.open2 = false)
+    }, null, 40, _hoisted_7),
+    createBaseVNode("quark-dialog", {
+      open: _ctx.open3,
+      title: _ctx.translate("dialogTitle"),
+      content: _ctx.translate("content"),
+      onClose: _cache[8] || (_cache[8] = ($event) => _ctx.open3 = false),
+      onConfirm: _cache[9] || (_cache[9] = ($event) => _ctx.open3 = false),
+      onCancel: _cache[10] || (_cache[10] = ($event) => _ctx.open3 = false)
+    }, null, 40, _hoisted_8),
+    createBaseVNode("quark-dialog", {
+      btnvertical: "",
+      open: _ctx.open6,
+      title: _ctx.translate("dialogTitle"),
+      content: _ctx.translate("content"),
+      onClose: _cache[11] || (_cache[11] = ($event) => _ctx.open6 = false),
+      onConfirm: _cache[12] || (_cache[12] = ($event) => _ctx.open6 = false),
+      onCancel: _cache[13] || (_cache[13] = ($event) => _ctx.open6 = false)
+    }, null, 40, _hoisted_9),
+    _hoisted_10,
+    createBaseVNode("quark-cell-group", null, [
+      createBaseVNode("quark-cell", {
+        islink: "",
+        title: _ctx.translate("customTitle"),
+        onClick: _cache[14] || (_cache[14] = ($event) => _ctx.open4 = true)
+      }, null, 8, _hoisted_11),
+      createBaseVNode("quark-cell", {
+        islink: "",
+        title: _ctx.translate("noTitle"),
+        onClick: _cache[15] || (_cache[15] = ($event) => _ctx.open5 = true)
+      }, null, 8, _hoisted_12)
+    ]),
+    createBaseVNode("quark-dialog", {
+      content: _ctx.translate("content"),
+      open: _ctx.open4,
+      onConfirm: _cache[16] || (_cache[16] = ($event) => _ctx.open4 = false),
+      onCancel: _cache[17] || (_cache[17] = ($event) => _ctx.open4 = false),
+      onClose: _cache[18] || (_cache[18] = ($event) => _ctx.open4 = false)
+    }, [
+      createBaseVNode("div", _hoisted_14, toDisplayString(_ctx.translate("customTitle")), 1)
+    ], 40, _hoisted_13),
+    createBaseVNode("quark-dialog", {
+      notitle: "",
+      open: _ctx.open5,
+      content: _ctx.translate("content"),
+      onConfirm: _cache[19] || (_cache[19] = ($event) => _ctx.open5 = false),
+      onCancel: _cache[20] || (_cache[20] = ($event) => _ctx.open5 = false),
+      onClose: _cache[21] || (_cache[21] = ($event) => _ctx.open5 = false)
+    }, null, 40, _hoisted_15),
+    _hoisted_16,
+    createBaseVNode("quark-cell-group", null, [
+      createBaseVNode("quark-cell", {
+        islink: "",
+        title: _ctx.translate("customFooter"),
+        onClick: _cache[22] || (_cache[22] = ($event) => _ctx.open8 = true)
+      }, null, 8, _hoisted_17),
+      createBaseVNode("quark-cell", {
+        islink: "",
+        title: _ctx.translate("noFooter"),
+        onClick: _cache[23] || (_cache[23] = ($event) => _ctx.open9 = true)
+      }, null, 8, _hoisted_18)
+    ]),
+    createBaseVNode("quark-dialog", {
+      title: _ctx.translate("dialogTitle"),
+      open: _ctx.open8,
+      onClose: _cache[25] || (_cache[25] = ($event) => _ctx.open8 = false)
+    }, [
+      createBaseVNode("div", null, toDisplayString(_ctx.translate("text")), 1),
+      createBaseVNode("div", _hoisted_20, [
+        createBaseVNode("quark-button", {
+          type: "primary",
+          onClick: _cache[24] || (_cache[24] = ($event) => _ctx.open8 = false)
+        }, toDisplayString(_ctx.translate("customFooter")), 1)
+      ])
+    ], 40, _hoisted_19),
+    createBaseVNode("quark-dialog", {
+      nofooter: "",
+      open: _ctx.open9,
+      title: _ctx.translate("dialogTitle"),
+      content: _ctx.translate("content"),
+      onConfirm: _cache[26] || (_cache[26] = ($event) => _ctx.open9 = false),
+      onClose: _cache[27] || (_cache[27] = ($event) => _ctx.open9 = false)
+    }, null, 40, _hoisted_21)
+  ]);
+}
+var demo = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { demo as default };
