f174c02561233e1ac457daf1ffa89892d9f5e64c
xxxsf <allan91coder@gmail.com>
2022-11-28 11:45:58 +0000

    Deploying to main from @ hellof2e/quark-design-docs@58e80b68050b7f0a0a992f0b5192434d6912cca1 🚀

---
+++ assets/doc-react.en-US.0a79334b.js
@@ -0,0 +1,26 @@
+import{d as a,c as n,a as t,o as e}from"./vue.9ec17576.js";const l={class:"markdown-body"},p=t(`<h1>Overlay</h1><h3>Intro</h3><p>Create a mask layer to emphasize specific page elements and prevent users from performing other operations.</p><h3>Install</h3><pre><code class="language-tsx"><span class="hljs-keyword">import</span> { Overlay } <span class="hljs-keyword">from</span> <span class="hljs-string">&quot;@quarkd/quark-react&quot;</span>;
+</code></pre><h3>Basic Usage</h3><pre><code class="language-js"><span class="hljs-keyword">export</span> <span class="hljs-keyword">default</span> () =&gt; {
+  <span class="hljs-keyword">const</span> [open, setOpen] = useState(<span class="hljs-literal">false</span>);
+
+  <span class="hljs-keyword">const</span> handleClose = <span class="hljs-function">() =&gt;</span> setOpen(<span class="hljs-literal">false</span>);
+  <span class="hljs-keyword">const</span> handleClick = <span class="hljs-function">() =&gt;</span> setOpen(<span class="hljs-literal">true</span>);
+
+  <span class="hljs-keyword">return</span> (
+    <span class="xml"><span class="hljs-tag">&lt;<span class="hljs-name">div</span>&gt;</span>
+      <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">onClick</span>=<span class="hljs-string">{handleClick}</span>&gt;</span><span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+      <span class="hljs-tag">&lt;<span class="hljs-name">Overlay</span> <span class="hljs-attr">open</span>=<span class="hljs-string">{open}</span> <span class="hljs-attr">onClose</span>=<span class="hljs-string">{handleClose}</span> /&gt;</span>
+    <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span></span>
+  );
+};
+</code></pre><h3>Embedded Content</h3><pre><code class="language-html"><span class="hljs-tag">&lt;<span class="hljs-name">Overlay</span> <span class="hljs-attr">open</span>=<span class="hljs-string">&quot;{open}&quot;</span> <span class="hljs-attr">onClose</span>=<span class="hljs-string">&quot;{handleClose}&quot;</span>&gt;</span>
+  <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">class</span>=<span class="hljs-string">&quot;content&quot;</span>&gt;</span><span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+<span class="hljs-tag">&lt;/<span class="hljs-name">Overlay</span>&gt;</span>
+<span class="hljs-tag">&lt;<span class="hljs-name">style</span>&gt;</span><span class="css">
+  <span class="hljs-selector-class">.content</span> {
+    <span class="hljs-attribute">border-radius</span>: <span class="hljs-number">4px</span>;
+    <span class="hljs-attribute">width</span>: <span class="hljs-number">200px</span>;
+    <span class="hljs-attribute">height</span>: <span class="hljs-number">200px</span>;
+    <span class="hljs-attribute">background-color</span>: white;
+  }
+</span><span class="hljs-tag">&lt;/<span class="hljs-name">style</span>&gt;</span>
+</code></pre><h2>API</h2><h3>Props</h3><table><thead><tr><th>Attribute</th><th>Description</th><th>Type</th><th>Default</th></tr></thead><tbody><tr><td>open</td><td>Whether to show picker</td><td><code class="">boolean</code></td><td><code class="">require</code></td></tr><tr><td>zindex</td><td>z-index</td><td><code class="">number </code></td><td><code class="">999</code></td></tr><tr><td>onClose</td><td>Close mask</td><td><code class="">() =&gt; void</code></td><td><code class="">require</code></td></tr></tbody></table><h2>CSS Variables</h2><p>The component provides the following<a href="https://developer.mozilla.org/zh-CN/docs/Web/CSS/Using_CSS_custom_properties">CSS variables</a>,which can be used to customize styles. Please refer to <a href="#/zh-CN/guide/theme">Theme customization</a>\u3002</p><table><thead><tr><th>Name</th><th>Description</th><th>Default Value</th></tr></thead><tbody><tr><td><code class="">--overlay-background</code></td><td>Mask background</td><td><code class="">rgba(0, 0, 0, 0.7)</code></td></tr></tbody></table>`,15),c=[p],i={},j="",g=a({__name:"doc-react.en-US",setup(o,{expose:s}){return s({frontmatter:{},excerpt:void 0}),(r,h)=>(e(),n("div",l,c))}});export{g as default,j as excerpt,i as frontmatter};
