393d6b2174b97008d82a4563140a297c4de3f7b6
xxxsf <allan91coder@gmail.com>
2022-12-05 12:03:10 +0000

    Deploying to main from @ hellof2e/quark-design-docs@425471e3f371e5db55fca57f88548f9733fa842e 🚀

---
+++ assets/doc-react.zh-CN.0639dcf8.js
@@ -0,0 +1,79 @@
+import{d as a,c as n,a as t,o as l}from"./vue.6c0d2727.js";const p={class:"markdown-body"},e=t(`<h1>ImagePreview \u56FE\u7247\u9884\u89C8</h1><h3>\u4ECB\u7ECD</h3><p>\u63D0\u4F9B\u57FA\u7840\u56FE\u7247\u9884\u89C8\u529F\u80FD</p><h3>\u5B89\u88C5\u4F7F\u7528</h3><p>\u5176\u4E2D <code class="">imagepreview</code> \u4E3A\u51FD\u6570\u5F0F\u7528\u6CD5\u3001<code class="">ImagePreview</code> \u4E3A\u7EC4\u4EF6\u5F0F\u7528\u6CD5</p><pre><code class="language-tsx"><span class="hljs-keyword">import</span> {
+  imagepreview,
+  ImagePreview,
+  ImagePreviewRef,
+} <span class="hljs-keyword">from</span> <span class="hljs-string">&quot;@quarkd/quark-react&quot;</span>;
+</code></pre><h3>\u57FA\u7840\u7528\u6CD5</h3><p>\u9884\u89C8\u56FE\u7247</p><pre><code class="language-js">imagepreview({
+  <span class="hljs-attr">images</span>: [<span class="hljs-string">&quot;1.png&quot;</span>, <span class="hljs-string">&quot;2.png&quot;</span>, <span class="hljs-string">&quot;3.png&quot;</span>],
+});
+</code></pre><h3>\u6307\u5B9A\u521D\u59CB\u4F4D\u7F6E</h3><p>\u6307\u5B9A\u56FE\u7247\u9884\u89C8\u4F4D\u7F6E\u9ED8\u8BA4\u4ECE 1 \u5F00\u59CB</p><pre><code class="language-js">imagepreview({
+  <span class="hljs-attr">startPosition</span>: <span class="hljs-number">3</span>,
+  <span class="hljs-attr">images</span>: [<span class="hljs-string">&quot;1.png&quot;</span>, <span class="hljs-string">&quot;2.png&quot;</span>, <span class="hljs-string">&quot;3.png&quot;</span>],
+});
+</code></pre><h3>\u76D1\u542C\u6ED1\u52A8\u4E8B\u4EF6</h3><pre><code class="language-js">imagepreview({
+  <span class="hljs-attr">images</span>: [<span class="hljs-string">&quot;1.png&quot;</span>, <span class="hljs-string">&quot;2.png&quot;</span>, <span class="hljs-string">&quot;3.png&quot;</span>],
+  <span class="hljs-attr">change</span>: <span class="hljs-function">(<span class="hljs-params">index</span>) =&gt;</span> Toast.text(<span class="hljs-string">\`\u5F53\u524D\u79FB\u52A8\u4F4D\u7F6E<span class="hljs-subst">\${index + <span class="hljs-number">1</span>}</span>\`</span>),
+});
+</code></pre><h3>\u76D1\u542C\u5173\u95ED\u4E8B\u4EF6</h3><pre><code class="language-js">imagepreview({
+  <span class="hljs-attr">images</span>: [<span class="hljs-string">&quot;1.png&quot;</span>, <span class="hljs-string">&quot;2.png&quot;</span>, <span class="hljs-string">&quot;3.png&quot;</span>],
+  <span class="hljs-attr">close</span>: <span class="hljs-function">(<span class="hljs-params">index</span>) =&gt;</span> Toast.text(<span class="hljs-string">\`\u5F53\u524D\u5173\u95ED\u4F4D\u7F6E<span class="hljs-subst">\${index + <span class="hljs-number">1</span>}</span>\`</span>),
+});
+</code></pre><h3>\u6807\u7B7E\u5F0F\u8C03\u7528</h3><pre><code class="language-tsx"><span class="hljs-keyword">export</span> <span class="hljs-keyword">default</span> () =&gt; {
+  <span class="hljs-keyword">const</span> preview = useRef&lt;ImagePreviewRef&gt;(<span class="hljs-literal">null</span>);
+  <span class="hljs-keyword">const</span> [open, setOpen] = useState(<span class="hljs-literal">false</span>);
+  <span class="hljs-keyword">const</span> baseUrls = [
+    <span class="hljs-string">&quot;https://m.hellobike.com/resource/helloyun/15697/iWS-0QI6QV.png&quot;</span>,
+    <span class="hljs-string">&quot;https://m.hellobike.com/resource/helloyun/15697/1V_2oJv02t.png&quot;</span>,
+    <span class="hljs-string">&quot;https://m.hellobike.com/resource/helloyun/15697/Q6t6B_noNetWork.png&quot;</span>,
+  ];
+
+  <span class="hljs-keyword">const</span> componentsClick = <span class="hljs-function">() =&gt;</span> {
+    setOpen(<span class="hljs-literal">true</span>);
+    preview.current.setData({
+      <span class="hljs-attr">images</span>: baseUrls,
+    });
+  };
+  <span class="hljs-keyword">return</span> (
+    <span class="xml"><span class="hljs-tag">&lt;<span class="hljs-name">div</span>&gt;</span>
+      <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">onClick</span>=<span class="hljs-string">{componentsClick}</span> /&gt;</span>
+      <span class="hljs-tag">&lt;<span class="hljs-name">ImagePreview</span> <span class="hljs-attr">ref</span>=<span class="hljs-string">{preview}</span> <span class="hljs-attr">open</span>=<span class="hljs-string">{open}</span> /&gt;</span>
+    <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span></span>
+  );
+};
+</code></pre><h3>\u81EA\u5B9A\u4E49\u5BFC\u822A</h3><p>\u914D\u5408 change \u4E8B\u4EF6\u663E\u793A\u4E0B\u6807</p><pre><code class="language-tsx"><span class="hljs-keyword">export</span> <span class="hljs-keyword">default</span> () =&gt; {
+  <span class="hljs-keyword">const</span> baseUrls = [
+    <span class="hljs-string">&quot;https://m.hellobike.com/resource/helloyun/15697/iWS-0QI6QV.png&quot;</span>,
+    <span class="hljs-string">&quot;https://m.hellobike.com/resource/helloyun/15697/1V_2oJv02t.png&quot;</span>,
+    <span class="hljs-string">&quot;https://m.hellobike.com/resource/helloyun/15697/Q6t6B_noNetWork.png&quot;</span>,
+  ];
+  <span class="hljs-keyword">const</span> [index, setIndex] = useState(<span class="hljs-number">0</span>);
+  <span class="hljs-keyword">const</span> [open2, setOpen2] = useState(<span class="hljs-literal">false</span>);
+  <span class="hljs-keyword">const</span> preview2 = useRef&lt;ImagePreviewRef&gt;(<span class="hljs-literal">null</span>);
+
+  <span class="hljs-keyword">const</span> componentsClick2 = <span class="hljs-function">() =&gt;</span> {
+    setOpen2(<span class="hljs-literal">true</span>);
+    preview2.current.setData({
+      <span class="hljs-attr">images</span>: baseUrls,
+      <span class="hljs-attr">change</span>: <span class="hljs-function">(<span class="hljs-params">_index</span>) =&gt;</span> setIndex(_index),
+      <span class="hljs-attr">close</span>: <span class="hljs-function">() =&gt;</span> setOpen2(<span class="hljs-literal">false</span>),
+    });
+  };
+
+  <span class="hljs-keyword">return</span> (
+    <span class="xml"><span class="hljs-tag">&lt;<span class="hljs-name">div</span>&gt;</span>
+      <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">onClick</span>=<span class="hljs-string">{componentsClick2}</span>&gt;</span>\u81EA\u5B9A\u4E49\u5BFC\u822A<span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+      <span class="hljs-tag">&lt;<span class="hljs-name">ImagePreview</span> <span class="hljs-attr">ref</span>=<span class="hljs-string">{preview2}</span> <span class="hljs-attr">open</span>=<span class="hljs-string">{open2}</span>&gt;</span>
+        <span class="hljs-tag">&lt;<span class="hljs-name">p</span> <span class="hljs-attr">className</span>=<span class="hljs-string">&quot;my-indicator&quot;</span> <span class="hljs-attr">slot</span>=<span class="hljs-string">&quot;indicator&quot;</span>&gt;</span>
+          \u7B2C{{ index }}\u9875
+        <span class="hljs-tag">&lt;/<span class="hljs-name">p</span>&gt;</span>
+      <span class="hljs-tag">&lt;/<span class="hljs-name">ImagePreview</span>&gt;</span>
+    <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span></span>
+  );
+};
+</code></pre><h2>Props</h2><table><thead><tr><th>\u53C2\u6570</th><th>\u8BF4\u660E</th><th>\u7C7B\u578B</th><th>\u9ED8\u8BA4\u503C</th></tr></thead><tbody><tr><td>open</td><td>\u6807\u7B7E\u5F0F\u8C03\u7528\u5C5E\u6027</td><td><code class="">boolean</code></td><td><code class="">false</code></td></tr></tbody></table><h3>\u65B9\u6CD5</h3><table><thead><tr><th>\u540D\u79F0</th><th>\u8BF4\u660E</th><th>\u7C7B\u578B</th></tr></thead><tbody><tr><td>setData</td><td>\u7528\u4E8E\u8BBE\u7F6E\u56FE\u7247\u9884\u89C8\u6570\u636E</td><td><code class="">(data: Options) =&gt; void</code></td></tr></tbody></table><h3>\u7C7B\u578B\u5B9A\u4E49</h3><pre><code class="language-js">type Options = {
+  <span class="hljs-attr">images</span>: string[] <span class="hljs-comment">// \u8981\u663E\u793A\u7684\u56FE\u7247\u6570\u7EC4</span>
+  startPosition?: number <span class="hljs-comment">// \u9ED8\u8BA4\u663E\u793A\u4F4D\u7F6E</span>
+  change?: <span class="hljs-function">(<span class="hljs-params">index: number</span>) =&gt;</span> <span class="hljs-keyword">void</span> <span class="hljs-comment">// \u56FE\u7247\u6ED1\u5230\u4E8B\u4EF6</span>
+  <span class="hljs-attr">close</span>: <span class="hljs-function">(<span class="hljs-params">index: number</span>) =&gt;</span> <span class="hljs-keyword">void</span> <span class="hljs-comment">// \u7EC4\u4EF6\u5173\u95ED\u4E8B\u4EF6</span>
+};
+</code></pre><h3>slot</h3><table><thead><tr><th>\u540D\u79F0</th><th>\u8BF4\u660E</th></tr></thead><tbody><tr><td>name=indicator</td><td>\u81EA\u5B9A\u4E49\u5206\u9875\u663E\u793A</td></tr></tbody></table>`,29),u=[e],i={},j="",g=a({__name:"doc-react.zh-CN",setup(c,{expose:s}){return s({frontmatter:{},excerpt:void 0}),(o,h)=>(l(),n("div",p,u))}});export{g as default,j as excerpt,i as frontmatter};
---
+++ assets/theme.zh-CN.fac975bb.js
@@ -0,0 +1,13 @@
+import{d as s,c as a,a as e,o}from"./vue.6c0d2727.js";const n={class:"markdown-body"},r=e(`<h1>\u4E3B\u9898\u5B9A\u5236</h1><p>Quark \u9ED8\u8BA4\u63D0\u4F9B\u4E00\u5957 UI \u4E3B\u9898\uFF0C\u540C\u65F6\u5141\u8BB8\u5728\u4E00\u5B9A\u7A0B\u5EA6\u4E0A\u5B9A\u5236\u65B0\u7684\u4E3B\u9898\uFF0C\u4EE5\u6EE1\u8DB3\u4E1A\u52A1\u7684\u591A\u6837\u5316\u89C6\u89C9\u9700\u6C42\u3002</p><div><br><img src="https://m.hellobike.com/resource/helloyun/13459/9FyYX_2.jpg?x-oss-process=image/quality,q_80" width="260" alt="quark \u4E3B\u9898\u5B9A\u5236"> \xA0\xA0\xA0\xA0\xA0\xA0 <img src="https://m.hellobike.com/resource/helloyun/13459/hrmSH_1.jpeg?x-oss-process=image/quality,q_80" width="260" alt="quark \u4E3B\u9898\u5B9A\u5236"><br></div><h2>\u6837\u5F0F\u53D8\u91CF</h2><p>\u4E0D\u540C\u4E8E\u4E00\u4E9B\u6846\u67B6\u91C7\u7528<code class="">less</code>\u3001<code class="">sass</code>\u7B49\u9884\u5904\u7406\u5668\uFF0C Quark \u91C7\u7528 <code class="">CSS</code><a href="https://developer.mozilla.org/zh-CN/docs/Web/CSS/Using_CSS_custom_properties">\u81EA\u5B9A\u4E49\u5C5E\u6027</a> \u5B9E\u73B0\u3002</p><p>\u60A8\u53EA\u9700\u8981\u5728\u60A8\u7684\u9879\u76EE\u4E2D\u8BBE\u7F6E\u4EE5\u4E0B\u53D8\u91CF\u5373\u53EF\u8F7B\u677E\u4FEE\u6539\u4E3B\u9898\u6837\u5F0F\uFF1A</p><pre><code class="language-less"><span class="hljs-selector-pseudo">:root</span> {
+  --quark-theme-<span class="hljs-attribute">color</span>: <span class="hljs-number">#0088ff</span>;
+  --quark-warning-<span class="hljs-attribute">color</span>: <span class="hljs-number">#fb990f</span>;
+  --quark-danger-<span class="hljs-attribute">color</span>: <span class="hljs-number">#ff5526</span>;
+  --quark-success-<span class="hljs-attribute">color</span>: <span class="hljs-number">#07c160</span>;
+}
+</code></pre><h2>\u5B9A\u5236\u4E3B\u9898</h2><p>\u4E3A\u7EC4\u4EF6\u5355\u72EC\u8BBE\u7F6E\u4E3B\u9898\u8272\uFF1A</p><pre><code class="language-less"><span class="hljs-selector-tag">quark-button</span> { 
+  --quark-theme-<span class="hljs-attribute">color</span>: <span class="hljs-number">#0088FF</span>;
+}
+</code></pre><p>\u5168\u5C40\u4E3A\u6240\u6709\u7EC4\u4EF6\u8BBE\u7F6E\u4E3B\u9898\u8272\uFF1A</p><pre><code class="language-less"><span class="hljs-selector-pseudo">:root</span> {
+  --quark-theme-<span class="hljs-attribute">color</span>: <span class="hljs-number">#0088ff</span>;
+}
+</code></pre>`,12),c=[r],h={},B="",d=s({__name:"theme.zh-CN",setup(t,{expose:u}){return u({frontmatter:{},excerpt:void 0}),(l,p)=>(o(),a("div",n,c))}});export{d as default,B as excerpt,h as frontmatter};
