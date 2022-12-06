d6a9c3b2cff27828e177d501eed636d31a7941c4
yuhaiyang1 <1281491236@qq.com>
2022-12-06 02:49:47 +0000

    Deploying to main from @ hellof2e/quark-design@e15ac4747153c15c87691feaef4a6c6696407fde 🚀

---
+++ demo/assets/demo.6cc78f4b.js
@@ -0,0 +1,222 @@
+var __defProp = Object.defineProperty;
+var __defProps = Object.defineProperties;
+var __getOwnPropDescs = Object.getOwnPropertyDescriptors;
+var __getOwnPropSymbols = Object.getOwnPropertySymbols;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __propIsEnum = Object.prototype.propertyIsEnumerable;
+var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
+var __spreadValues = (a, b) => {
+  for (var prop in b || (b = {}))
+    if (__hasOwnProp.call(b, prop))
+      __defNormalProp(a, prop, b[prop]);
+  if (__getOwnPropSymbols)
+    for (var prop of __getOwnPropSymbols(b)) {
+      if (__propIsEnum.call(b, prop))
+        __defNormalProp(a, prop, b[prop]);
+    }
+  return a;
+};
+var __spreadProps = (a, b) => __defProps(a, __getOwnPropDescs(b));
+var __async = (__this, __arguments, generator) => {
+  return new Promise((resolve, reject) => {
+    var fulfilled = (value) => {
+      try {
+        step(generator.next(value));
+      } catch (e) {
+        reject(e);
+      }
+    };
+    var rejected = (value) => {
+      try {
+        step(generator.throw(value));
+      } catch (e) {
+        reject(e);
+      }
+    };
+    var step = (x) => x.done ? resolve(x.value) : Promise.resolve(x.value).then(fulfilled, rejected);
+    step((generator = generator.apply(__this, __arguments)).next());
+  });
+};
+import { c as createComponent } from "./component.4c33c442.js";
+import { _ as _export_sfc, r as ref, o as onBeforeMount, u as useTranslate, f as onMounted, T as Toast, c as createElementBlock, a as createBaseVNode, t as toDisplayString, b as openBlock } from "./mobile.ab3cc059.js";
+import { o as onBeforeRouteLeave } from "./vue-router.73c91165.js";
+var demo_scss_vue_type_style_index_0_src_lang = "";
+const { createDemo, translate } = createComponent("uploader");
+const _sfc_main = createDemo({
+  setup() {
+    const isPreview = ref(false);
+    const preview = ref(null);
+    const preview2 = ref(null);
+    const before = ref(null);
+    const previewUrls = [
+      "https://m.hellobike.com/resource/helloyun/15697/9VgwC_Screenshot_20220215_191457_com.jingyao.easybike.jpg?x-oss-process=image/quality,q_80",
+      "https://m.hellobike.com/resource/helloyun/15697/iWS-0QI6QV.png"
+    ];
+    onBeforeMount(() => {
+      useTranslate({
+        "zh-CN": {
+          basic: "\u57FA\u7840\u7528\u6CD5",
+          preview: "\u6587\u4EF6\u9884\u89C8",
+          previewMode: "\u9884\u89C8\u6A21\u5F0F",
+          limit: "\u9650\u5236\u4E0A\u4F20\u6570\u91CF",
+          size: "\u9650\u5236\u4E0A\u4F20\u5927\u5C0F",
+          custom: "\u81EA\u5B9A\u4E49\u4E0A\u4F20\u6837\u5F0F",
+          file: "\u4E0A\u4F20\u6587\u4EF6",
+          before: "\u4E0A\u4F20\u524D\u7F6E",
+          disabled: "\u7981\u6B62\u4E0A\u4F20",
+          toast: {
+            format: "\u8BF7\u4E0A\u4F20 jpg \u683C\u5F0F\u56FE\u7247",
+            overSize: "\u6709\u6587\u4EF6\u8D85\u8FC71KB\u4E86\u54E6"
+          }
+        },
+        "en-US": {
+          basic: "Basic Usage",
+          preview: "File Preview",
+          previewMode: "File Preview Mode",
+          limit: "Limit Uploads Number",
+          size: "Limit Uploads Size",
+          custom: "Custom Upload Style",
+          file: "Upload File",
+          before: "Before Uploading",
+          disabled: "Disabled",
+          toast: {
+            format: "Please upload image in jpg format",
+            overSize: "There are files over 1KB"
+          }
+        }
+      });
+    });
+    onMounted(() => {
+      preview.value.setPreview(previewUrls);
+      preview2.value.setPreview(previewUrls);
+      before.value.beforeUpload = beforeUpload;
+    });
+    const sleep = (time) => {
+      return new Promise((reslove) => {
+        setTimeout(() => {
+          reslove(true);
+        }, time);
+      });
+    };
+    const beforeUpload = (files) => {
+      const r = files.every((file) => file.type === "image/jpg");
+      if (!r) {
+        Toast.text(`${translate("toast.format")}`);
+        return false;
+      }
+      return true;
+    };
+    const oversize = () => {
+      Toast.text(`${translate("toast.overSize")}`);
+    };
+    const uploadAction = (item) => __async(this, null, function* () {
+      preview.value.setStatus(__spreadProps(__spreadValues({}, item), {
+        status: "uploading",
+        message: "\u4E0A\u4F20\u4E2D"
+      }));
+      yield sleep(2e3);
+      preview.value.setStatus(__spreadProps(__spreadValues({}, item), {
+        status: "done"
+      }));
+      Toast.success("\u4E0A\u4F20\u6210\u529F");
+    });
+    const afterRead = (_0) => __async(this, [_0], function* ({ detail: file }) {
+      if (Array.isArray(file)) {
+        for (let i = 0; i < file.length; i++) {
+          const item = file[i];
+          uploadAction(item);
+        }
+      } else {
+        uploadAction(file);
+      }
+    });
+    onBeforeRouteLeave(() => {
+      const nodes = document.querySelectorAll("quark-image-preview ");
+      nodes.forEach((i) => i.open = false);
+    });
+    return {
+      isPreview,
+      preview,
+      preview2,
+      before,
+      beforeUpload,
+      oversize,
+      afterRead,
+      translate
+    };
+  }
+});
+const _hoisted_1 = { class: "demo uploader-demo" };
+const _hoisted_2 = { class: "flex" };
+const _hoisted_3 = ["preview"];
+const _hoisted_4 = { class: "flex" };
+const _hoisted_5 = /* @__PURE__ */ createBaseVNode("div", { class: "flex" }, [
+  /* @__PURE__ */ createBaseVNode("quark-uploader", {
+    maxcount: "2",
+    preview: ""
+  })
+], -1);
+const _hoisted_6 = { class: "flex" };
+const _hoisted_7 = { preview: "" };
+const _hoisted_8 = {
+  type: "primary",
+  slot: "uploader",
+  icon: "home"
+};
+const _hoisted_9 = {
+  preview: "",
+  ref: "before"
+};
+const _hoisted_10 = /* @__PURE__ */ createBaseVNode("quark-uploader", {
+  preview: "",
+  disabled: true
+}, null, -1);
+const _hoisted_11 = { class: "flex" };
+const _hoisted_12 = {
+  ref: "preview2",
+  preview: "",
+  readonly: ""
+};
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", _hoisted_1, [
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("basic")), 1),
+    createBaseVNode("div", _hoisted_2, [
+      createBaseVNode("quark-uploader", {
+        onAfterread: _cache[0] || (_cache[0] = (...args) => _ctx.afterRead && _ctx.afterRead(...args)),
+        preview: _ctx.isPreview
+      }, null, 40, _hoisted_3)
+    ]),
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("preview")), 1),
+    createBaseVNode("div", _hoisted_4, [
+      createBaseVNode("quark-uploader", {
+        onAfterread: _cache[1] || (_cache[1] = (...args) => _ctx.afterRead && _ctx.afterRead(...args)),
+        ref: "preview",
+        preview: ""
+      }, null, 544)
+    ]),
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("limit")), 1),
+    _hoisted_5,
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("size")), 1),
+    createBaseVNode("quark-uploader", {
+      maxsize: "1024",
+      onOversize: _cache[2] || (_cache[2] = (...args) => _ctx.oversize && _ctx.oversize(...args)),
+      ref: "oversize"
+    }, null, 544),
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("custom")), 1),
+    createBaseVNode("div", _hoisted_6, [
+      createBaseVNode("quark-uploader", _hoisted_7, [
+        createBaseVNode("quark-button", _hoisted_8, toDisplayString(_ctx.translate("file")), 1)
+      ])
+    ]),
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("before")), 1),
+    createBaseVNode("quark-uploader", _hoisted_9, null, 512),
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("disabled")), 1),
+    _hoisted_10,
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("previewMode")), 1),
+    createBaseVNode("div", _hoisted_11, [
+      createBaseVNode("quark-uploader", _hoisted_12, null, 512)
+    ])
+  ]);
+}
+var demo = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { demo as default };
---
+++ demo/assets/demo.ca6c152b.js
@@ -0,0 +1,211 @@
+import { _ as _export_sfc, r as ref, o as onBeforeMount, u as useTranslate, f as onMounted, c as createElementBlock, a as createBaseVNode, t as toDisplayString, T as Toast, b as openBlock } from "./mobile.ab3cc059.js";
+import { c as createComponent } from "./component.4c33c442.js";
+var demo_scss_vue_type_style_index_0_src_lang = "";
+const { createDemo, translate } = createComponent("popover");
+const _sfc_main = createDemo({
+  setup() {
+    const data = ref({
+      light: false,
+      dark: false,
+      icon: false,
+      disable: false,
+      content: false
+    });
+    const lightRef = ref(null);
+    const darkRef = ref(null);
+    const iconRef = ref(null);
+    const disableRef = ref(null);
+    onBeforeMount(() => {
+      useTranslate({
+        "zh-CN": {
+          basicUsage: "\u57FA\u7840\u7528\u6CD5",
+          lightStyle: "\u6D45\u8272\u98CE\u683C",
+          darkStyle: "\u6DF1\u8272\u98CE\u683C",
+          settings: "\u9009\u9879\u914D\u7F6E",
+          showIcon: "\u5C55\u793A\u56FE\u6807",
+          disable: "\u7981\u7528\u72B6\u6001",
+          customContent: "\u81EA\u5B9A\u4E49\u5185\u5BB9",
+          content: "\u6211\u662F\u81EA\u5B9A\u4E49\u5185\u5BB9",
+          options: ["\u9009\u9879\u4E00", "\u9009\u9879\u4E8C", "\u9009\u9879\u4E09"]
+        },
+        "en-US": {
+          basicUsage: "Basic Usage",
+          lightStyle: "Light Style",
+          darkStyle: "Dark Style",
+          settings: "Option Settings",
+          showIcon: "Show Icon",
+          disable: "disabled",
+          customContent: "Custom Content",
+          content: "This is custom content",
+          options: ["Option 1", "Option 2", "Option 3"]
+        }
+      });
+    });
+    onMounted(() => {
+      const actions = [
+        {
+          text: `${translate("options")[0]}`
+        },
+        {
+          text: `${translate("options")[1]}`
+        },
+        {
+          text: `${translate("options")[2]}`
+        }
+      ];
+      lightRef.value.setActions(actions);
+      darkRef.value.setActions(actions);
+      iconRef.value.setActions([
+        {
+          text: `${translate("options")[0]}`
+        },
+        {
+          text: `${translate("options")[1]}`,
+          icon: "tel"
+        },
+        {
+          text: `${translate("options")[2]}`,
+          icon: "https://m.hellobike.com/resource/helloyun/18625/WUu02_img.png"
+        }
+      ]);
+      disableRef.value.setActions([
+        {
+          text: `${translate("options")[0]}`,
+          disabled: true
+        },
+        {
+          text: `${translate("options")[1]}`
+        },
+        {
+          text: `${translate("options")[2]}`
+        }
+      ]);
+    });
+    const handleLightSelect = ({ detail }) => {
+      const { action, index } = detail;
+      Toast.text(action.text);
+      close("light");
+    };
+    const handleDarkSelect = ({ detail }) => {
+      const { action, index } = detail;
+      Toast.text(action.text);
+      close("dark");
+    };
+    const handleIconSelect = ({ detail }) => {
+      const { action, index } = detail;
+      Toast.text(action.text);
+      close("icon");
+    };
+    const handleDisableSelect = ({ detail }) => {
+      const { action, index } = detail;
+      Toast.text(action.text);
+      close("disable");
+    };
+    const open = (name) => {
+      data.value[name] = true;
+      data.value.test = true;
+    };
+    const close = (name) => {
+      data.value[name] = false;
+    };
+    return {
+      translate,
+      data,
+      lightRef,
+      darkRef,
+      iconRef,
+      disableRef,
+      handleLightSelect,
+      handleDarkSelect,
+      handleIconSelect,
+      handleDisableSelect,
+      open,
+      close
+    };
+  }
+});
+const _hoisted_1 = { class: "demo" };
+const _hoisted_2 = ["open"];
+const _hoisted_3 = ["open"];
+const _hoisted_4 = ["open"];
+const _hoisted_5 = ["open"];
+const _hoisted_6 = ["open"];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", _hoisted_1, [
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("basicUsage")), 1),
+    createBaseVNode("quark-popover", {
+      scroolhidden: "",
+      open: _ctx.data.light,
+      theme: "light",
+      placement: "bottomleft",
+      ref: "lightRef",
+      onClose: _cache[1] || (_cache[1] = ($event) => _ctx.close("light")),
+      onSelect: _cache[2] || (_cache[2] = (...args) => _ctx.handleLightSelect && _ctx.handleLightSelect(...args))
+    }, [
+      createBaseVNode("div", {
+        class: "quark-popover",
+        onClick: _cache[0] || (_cache[0] = ($event) => _ctx.open("light"))
+      }, toDisplayString(_ctx.translate("lightStyle")), 1)
+    ], 40, _hoisted_2),
+    createBaseVNode("quark-popover", {
+      scroolhidden: "",
+      open: _ctx.data.dark,
+      theme: "dark",
+      ref: "darkRef",
+      onClose: _cache[4] || (_cache[4] = ($event) => _ctx.close("dark")),
+      onSelect: _cache[5] || (_cache[5] = (...args) => _ctx.handleDarkSelect && _ctx.handleDarkSelect(...args))
+    }, [
+      createBaseVNode("div", {
+        class: "quark-popover",
+        onClick: _cache[3] || (_cache[3] = ($event) => _ctx.open("dark"))
+      }, toDisplayString(_ctx.translate("darkStyle")), 1)
+    ], 40, _hoisted_3),
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("settings")), 1),
+    createBaseVNode("quark-popover", {
+      scroolhidden: "",
+      open: _ctx.data.icon,
+      placement: "bottomleft",
+      ref: "iconRef",
+      theme: "light",
+      onClose: _cache[7] || (_cache[7] = ($event) => _ctx.close("icon")),
+      onSelect: _cache[8] || (_cache[8] = (...args) => _ctx.handleIconSelect && _ctx.handleIconSelect(...args))
+    }, [
+      createBaseVNode("div", {
+        class: "quark-popover",
+        onClick: _cache[6] || (_cache[6] = ($event) => _ctx.open("icon"))
+      }, toDisplayString(_ctx.translate("showIcon")), 1)
+    ], 40, _hoisted_4),
+    createBaseVNode("quark-popover", {
+      scroolhidden: "",
+      theme: "light",
+      open: _ctx.data.disable,
+      ref: "disableRef",
+      onClose: _cache[10] || (_cache[10] = ($event) => _ctx.close("disable")),
+      onSelect: _cache[11] || (_cache[11] = (...args) => _ctx.handleDisableSelect && _ctx.handleDisableSelect(...args))
+    }, [
+      createBaseVNode("div", {
+        class: "quark-popover",
+        onClick: _cache[9] || (_cache[9] = ($event) => _ctx.open("disable"))
+      }, toDisplayString(_ctx.translate("disable")), 1)
+    ], 40, _hoisted_5),
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("")), 1),
+    createBaseVNode("quark-popover", {
+      scroolhidden: "",
+      open: _ctx.data.content,
+      placement: "bottomleft",
+      onClose: _cache[14] || (_cache[14] = ($event) => _ctx.close("content"))
+    }, [
+      createBaseVNode("div", {
+        class: "quark-popover",
+        onClick: _cache[12] || (_cache[12] = ($event) => _ctx.open("content"))
+      }, toDisplayString(_ctx.translate("customContent")), 1),
+      createBaseVNode("div", {
+        slot: "content",
+        class: "quark-popover-content",
+        onClick: _cache[13] || (_cache[13] = ($event) => _ctx.close("content"))
+      }, toDisplayString(_ctx.translate("content")), 1)
+    ], 40, _hoisted_6)
+  ]);
+}
+var demo = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { demo as default };
