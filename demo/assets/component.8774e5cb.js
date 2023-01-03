79a4ba3d54beee5af5a00407b0632b9d3b906959
sanqi-med <sanqi_med@163.com>
2023-01-03 06:45:15 +0000

    Deploying to main from @ hellof2e/quark-design@801e4e1cbaadc1dc2378396f0d2d3e52eae22ca1 🚀

---
+++ demo/assets/component.8774e5cb.js
@@ -0,0 +1,26 @@
+import { L as Locale, d as defineComponent } from "./mobile.98e90c24.js";
+const isFunction = (val) => typeof val === "function";
+const getPropByPath = (obj, keyPath) => {
+  try {
+    return keyPath.split(".").reduce((prev, curr) => prev[curr], obj);
+  } catch (error) {
+    return "";
+  }
+};
+function createComponent(name) {
+  const componentName = "quark-" + name;
+  return {
+    componentName,
+    translate(keyPath, ...args) {
+      const languages = Locale.languages();
+      const text = getPropByPath(languages, `${name.replace("-", "")}.${keyPath}`) || getPropByPath(languages, keyPath);
+      return isFunction(text) ? text(...args) : text;
+    },
+    createDemo: function(_component) {
+      _component.baseName = name;
+      _component.name = "demo-" + name;
+      return defineComponent(_component);
+    }
+  };
+}
+export { createComponent as c };
