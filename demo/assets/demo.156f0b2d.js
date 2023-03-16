177fcb1a370107bb8b9c9f178e23bb0826496a8e
xxxsf <allan91coder@gmail.com>
2023-03-16 08:07:47 +0000

    Deploying to main from @ hellof2e/quark-design@7fdcc49a11220bb403c27d15d78a45acf20e6f58 🚀

---
+++ demo/assets/demo.156f0b2d.js
@@ -0,0 +1,91 @@
+import { _ as _export_sfc, o as onBeforeMount, u as useTranslate, c as createElementBlock, a as createBaseVNode, t as toDisplayString, b as openBlock } from "./mobile.b0f3b686.js";
+import { c as createComponent } from "./component.8551d1fc.js";
+var demo_scss_vue_type_style_index_0_src_lang = "";
+const { createDemo, translate } = createComponent("loading");
+const _sfc_main = createDemo({
+  setup() {
+    onBeforeMount(() => {
+      useTranslate({
+        "zh-CN": {
+          loadingType: "\u52A0\u8F7D\u7C7B\u578B",
+          customSize: "\u81EA\u5B9A\u4E49\u5927\u5C0F",
+          customColor: "\u81EA\u5B9A\u4E49\u989C\u8272",
+          loadingText: "\u52A0\u8F7D\u6587\u6848",
+          vertical: "\u5782\u76F4\u6392\u5217",
+          customStyle: "\u81EA\u5B9A\u4E49\u6587\u672C\u6837\u5F0F",
+          load: "\u52A0\u8F7D\u4E2D..."
+        },
+        "en-US": {
+          loadingType: "Loading Type",
+          customSize: "Custom Size",
+          customColor: "Custom Color",
+          loadingText: "Loading Text",
+          vertical: "Vertical Layout",
+          customStyle: "Custom Text Style",
+          load: "loading..."
+        }
+      });
+    });
+    return {
+      translate
+    };
+  }
+});
+const _hoisted_1 = { class: "demo loading-demo" };
+const _hoisted_2 = /* @__PURE__ */ createBaseVNode("div", { class: "demo-loadings" }, [
+  /* @__PURE__ */ createBaseVNode("quark-loading", { type: "circular" }),
+  /* @__PURE__ */ createBaseVNode("quark-loading")
+], -1);
+const _hoisted_3 = /* @__PURE__ */ createBaseVNode("div", { class: "demo-loadings" }, [
+  /* @__PURE__ */ createBaseVNode("quark-loading", { size: "20" }),
+  /* @__PURE__ */ createBaseVNode("quark-loading", { size: "30" }),
+  /* @__PURE__ */ createBaseVNode("quark-loading", { size: "40" })
+], -1);
+const _hoisted_4 = /* @__PURE__ */ createBaseVNode("div", { class: "demo-loadings" }, [
+  /* @__PURE__ */ createBaseVNode("quark-loading", { size: "30" }),
+  /* @__PURE__ */ createBaseVNode("quark-loading", {
+    size: "30",
+    color: "green"
+  }),
+  /* @__PURE__ */ createBaseVNode("quark-loading", {
+    size: "30",
+    color: "warning"
+  })
+], -1);
+const _hoisted_5 = { class: "demo-loadings" };
+const _hoisted_6 = { size: "30" };
+const _hoisted_7 = { class: "demo-loadings" };
+const _hoisted_8 = {
+  size: "30",
+  vertical: ""
+};
+const _hoisted_9 = { class: "demo-loadings" };
+const _hoisted_10 = {
+  size: "30",
+  vertical: "",
+  class: "custom-text"
+};
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", _hoisted_1, [
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("loadingType")), 1),
+    _hoisted_2,
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("customSize")), 1),
+    _hoisted_3,
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("customColor")), 1),
+    _hoisted_4,
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("loadingText")), 1),
+    createBaseVNode("div", _hoisted_5, [
+      createBaseVNode("quark-loading", _hoisted_6, toDisplayString(_ctx.translate("load")), 1)
+    ]),
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("vertical")), 1),
+    createBaseVNode("div", _hoisted_7, [
+      createBaseVNode("quark-loading", _hoisted_8, toDisplayString(_ctx.translate("load")), 1)
+    ]),
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("customStyle")), 1),
+    createBaseVNode("div", _hoisted_9, [
+      createBaseVNode("quark-loading", _hoisted_10, toDisplayString(_ctx.translate("load")), 1)
+    ])
+  ]);
+}
+var demo = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { demo as default };
---
+++ demo/assets/demo.6fe210d5.js
@@ -0,0 +1,134 @@
+import { c as createComponent } from "./component.8551d1fc.js";
+import { _ as _export_sfc, r as ref, o as onBeforeMount, u as useTranslate, c as createElementBlock, a as createBaseVNode, t as toDisplayString, b as openBlock } from "./mobile.b0f3b686.js";
+var demo_scss_vue_type_style_index_0_src_lang = "";
+const { createDemo, translate } = createComponent("cell");
+const _sfc_main = createDemo({
+  setup() {
+    const fruit = ref("apple");
+    const onChange = ({ detail }) => {
+      fruit.value = detail.value;
+    };
+    onBeforeMount(() => {
+      useTranslate({
+        "zh-CN": {
+          CellTitle: {
+            basicUsage: "\u57FA\u672C\u7528\u6CD5",
+            router: "\u94FE\u63A5\u8DF3\u8F6C",
+            icon: "\u5C55\u793A\u56FE\u6807",
+            group: "\u5206\u7EC4\u7528\u6CD5",
+            custom: "\u81EA\u5B9A\u4E49\u6837\u5F0F\uFF0C\u7701\u7565\u53F7\u5C55\u793A",
+            right: "\u81EA\u5B9A\u4E49\u53F3\u4FA7\u63CF\u8FF0"
+          },
+          title: "\u8FD9\u662F\u6807\u9898",
+          desc: "\u63CF\u8FF0\u6587\u5B57",
+          longTitle: "\u8FD9\u662F\u6807\u9898\u975E\u5E38\u7684\u957F\uFF0C\u592A\u957F\u4F1A\u81EA\u52A8\u6362\u884C\u7684\u54E6",
+          longLongTitle: "\u8FD9\u662F\u6807\u9898\u5F88\u957F\u957F\u957F\u957F\u957F\u957F\u957F\u957F\u957F\u957F\u957F\u957F\u957F\u957F\u957F\u957F\u957F\u957F\u957F\u957F\u957F\u957F\u957F\u957F",
+          router: "\u8DEF\u7531\u8DF3\u8F6C",
+          url: "\u94FE\u63A5\u8DF3\u8F6C"
+        },
+        "en-US": {
+          CellTitle: {
+            basicUsage: "Basic Usage",
+            router: "Router",
+            icon: "Icon",
+            group: "Inset Grouped",
+            custom: "Custom Style",
+            right: "Right content"
+          },
+          title: "Cell Title",
+          desc: "Description",
+          longTitle: "This is a very long title, it will automatically wrap.",
+          longLongTitle: "The title is very very very very very very very long",
+          router: "Router",
+          url: "Url"
+        }
+      });
+    });
+    return {
+      fruit,
+      translate,
+      onChange
+    };
+  }
+});
+const _hoisted_1 = { class: "demo no-padding demo-cell" };
+const _hoisted_2 = ["title", "desc"];
+const _hoisted_3 = ["title"];
+const _hoisted_4 = ["title", "desc"];
+const _hoisted_5 = ["title", "desc"];
+const _hoisted_6 = ["title"];
+const _hoisted_7 = ["title"];
+const _hoisted_8 = ["title", "desc"];
+const _hoisted_9 = ["title"];
+const _hoisted_10 = ["title"];
+const _hoisted_11 = ["title"];
+const _hoisted_12 = ["title"];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", _hoisted_1, [
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("CellTitle.basicUsage")), 1),
+    createBaseVNode("quark-cell-group", null, [
+      createBaseVNode("quark-cell", {
+        title: _ctx.translate("title"),
+        desc: _ctx.translate("desc")
+      }, null, 8, _hoisted_2),
+      createBaseVNode("quark-cell", {
+        title: _ctx.translate("title"),
+        islink: ""
+      }, null, 8, _hoisted_3),
+      createBaseVNode("quark-cell", {
+        title: _ctx.translate("title"),
+        desc: _ctx.translate("desc"),
+        islink: ""
+      }, null, 8, _hoisted_4),
+      createBaseVNode("quark-cell", {
+        title: _ctx.translate("longTitle"),
+        desc: _ctx.translate("desc"),
+        islink: ""
+      }, null, 8, _hoisted_5)
+    ]),
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("CellTitle.router")), 1),
+    createBaseVNode("quark-cell-group", null, [
+      createBaseVNode("quark-cell", {
+        title: _ctx.translate("router"),
+        to: "#/button",
+        islink: ""
+      }, null, 8, _hoisted_6),
+      createBaseVNode("quark-cell", {
+        title: _ctx.translate("url"),
+        to: "https://baidu.com",
+        islink: ""
+      }, null, 8, _hoisted_7)
+    ]),
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("CellTitle.icon")), 1),
+    createBaseVNode("quark-cell-group", null, [
+      createBaseVNode("quark-cell", {
+        title: _ctx.translate("title"),
+        desc: _ctx.translate("desc"),
+        islink: "",
+        icon: "https://m.hellobike.com/resource/helloyun/18625/WUu02_img.png?x-oss-process=image/quality,q_80"
+      }, null, 8, _hoisted_8)
+    ]),
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("CellTitle.group")), 1),
+    createBaseVNode("quark-cell-group", null, [
+      createBaseVNode("quark-cell", {
+        title: _ctx.translate("title")
+      }, null, 8, _hoisted_9),
+      createBaseVNode("quark-cell", {
+        title: _ctx.translate("title")
+      }, null, 8, _hoisted_10)
+    ]),
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("CellTitle.custom")), 1),
+    createBaseVNode("quark-cell", {
+      class: "my-cell",
+      title: _ctx.translate("longLongTitle")
+    }, null, 8, _hoisted_11),
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("CellTitle.right")), 1),
+    createBaseVNode("quark-cell", {
+      title: _ctx.translate("title")
+    }, [
+      createBaseVNode("div", null, toDisplayString(_ctx.translate("CellTitle.right")), 1)
+    ], 8, _hoisted_12)
+  ]);
+}
+var demo = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { demo as default };
