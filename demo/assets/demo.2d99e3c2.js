5ca0a97a6ce7e04d86e62b7792179216616144ef
xxxsf <allan91coder@gmail.com>
2023-03-16 09:10:26 +0000

    Deploying to main from @ hellof2e/quark-design@13e3972e51e86e2a0f4ad48db089657410bee626 🚀

---
+++ demo/assets/demo.2d99e3c2.js
@@ -0,0 +1,163 @@
+import { c as createComponent } from "./component.a18d28e2.js";
+import { _ as _export_sfc, r as ref, o as onBeforeMount, u as useTranslate, c as createElementBlock, a as createBaseVNode, t as toDisplayString, b as openBlock } from "./mobile.6eaf977a.js";
+var demo_scss_vue_type_style_index_0_src_lang = "";
+const { createDemo, translate } = createComponent("tooltip");
+const _sfc_main = createDemo({
+  setup() {
+    const data = ref({
+      top: false,
+      topleft: false,
+      topright: false,
+      bottom: false,
+      bottomleft: false,
+      bottomright: false,
+      left: false,
+      lefttop: false,
+      leftbottom: false,
+      right: false,
+      righttop: false,
+      rightbottom: false,
+      topclose: false,
+      autoclose: false,
+      customstyle: false
+    });
+    onBeforeMount(() => {
+      useTranslate({
+        "zh-CN": {
+          tipsPosition: "Tips \u4F4D\u7F6E\uFF08\u6240\u6709\u652F\u6301\u7684\u4F4D\u7F6E\u53C2\u89C1placement \u53C2\u6570\uFF09",
+          topPosition: "top \u4F4D\u7F6E",
+          topLeftPosition: "top left \u4F4D\u7F6E",
+          topRightPosition: "top right \u4F4D\u7F6E",
+          tips: "\u6C14\u6CE1\u6587\u5B57",
+          showCloseButton: "\u663E\u793A\u5173\u95ED\u6309\u94AE",
+          setAutoOffTime: "\u8BBE\u7F6E\u81EA\u52A8\u5173\u95ED\u65F6\u95F4",
+          customStyle: "\u81EA\u5B9A\u4E49\u6837\u5F0F"
+        },
+        "en-US": {
+          tipsPosition: "Tips Position(See placement parameter for all supported positions)",
+          topPosition: "Top Position",
+          topLeftPosition: "Top Left Position",
+          topRightPosition: "Top Right Position",
+          tips: "Bubble Text",
+          shoeCloseButton: "Show Close Button",
+          setAutoOffTime: "Set auto-off time",
+          customStyle: "Custom Style"
+        }
+      });
+    });
+    const open = (placement) => {
+      data.value[placement] = true;
+    };
+    const close = (placement) => {
+      data.value[placement] = false;
+    };
+    return {
+      data,
+      open,
+      close,
+      translate
+    };
+  }
+});
+const _hoisted_1 = { class: "demo" };
+const _hoisted_2 = { class: "quark-tip" };
+const _hoisted_3 = ["tips", "open"];
+const _hoisted_4 = { class: "quark-tip" };
+const _hoisted_5 = ["tips", "open"];
+const _hoisted_6 = { class: "quark-tip" };
+const _hoisted_7 = ["tips", "open"];
+const _hoisted_8 = { class: "quark-tip" };
+const _hoisted_9 = ["tips", "open"];
+const _hoisted_10 = { class: "quark-tip" };
+const _hoisted_11 = ["tips", "open"];
+const _hoisted_12 = { class: "quark-tip" };
+const _hoisted_13 = ["tips", "open"];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", _hoisted_1, [
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("tipsPosition")), 1),
+    createBaseVNode("div", _hoisted_2, [
+      createBaseVNode("quark-tooltip", {
+        placement: "top",
+        tips: _ctx.translate("tips"),
+        scroolhidden: "",
+        open: _ctx.data.top,
+        onClick: _cache[0] || (_cache[0] = ($event) => _ctx.open("top")),
+        onClose: _cache[1] || (_cache[1] = ($event) => _ctx.close("top"))
+      }, [
+        createBaseVNode("div", null, toDisplayString(_ctx.translate("topPosition")), 1)
+      ], 40, _hoisted_3)
+    ]),
+    createBaseVNode("div", _hoisted_4, [
+      createBaseVNode("quark-tooltip", {
+        placement: "topleft",
+        tips: _ctx.translate("tips"),
+        scroolhidden: "",
+        open: _ctx.data.topleft,
+        onClick: _cache[2] || (_cache[2] = ($event) => _ctx.open("topleft")),
+        onClose: _cache[3] || (_cache[3] = ($event) => _ctx.close("topleft"))
+      }, [
+        createBaseVNode("div", null, toDisplayString(_ctx.translate("topLeftPosition")), 1)
+      ], 40, _hoisted_5)
+    ]),
+    createBaseVNode("div", _hoisted_6, [
+      createBaseVNode("quark-tooltip", {
+        placement: "topright",
+        tips: _ctx.translate("tips"),
+        scroolhidden: "",
+        open: _ctx.data.topright,
+        onClick: _cache[4] || (_cache[4] = ($event) => _ctx.open("topright")),
+        onClose: _cache[5] || (_cache[5] = ($event) => _ctx.close("topright"))
+      }, [
+        createBaseVNode("div", null, toDisplayString(_ctx.translate("topRightPosition")), 1)
+      ], 40, _hoisted_7)
+    ]),
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("showCloseButton")), 1),
+    createBaseVNode("div", _hoisted_8, [
+      createBaseVNode("quark-tooltip", {
+        placement: "top",
+        closeable: "",
+        scroolhidden: "",
+        tips: _ctx.translate("tips"),
+        open: _ctx.data.topclose,
+        onClick: _cache[6] || (_cache[6] = ($event) => _ctx.open("topclose")),
+        onClose: _cache[7] || (_cache[7] = ($event) => _ctx.close("topclose"))
+      }, [
+        createBaseVNode("div", null, toDisplayString(_ctx.translate("topPosition")), 1)
+      ], 40, _hoisted_9)
+    ]),
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("setAutoOffTime")), 1),
+    createBaseVNode("div", _hoisted_10, [
+      createBaseVNode("quark-tooltip", {
+        placement: "top",
+        autoclose: "",
+        opentime: "5000",
+        scroolhidden: "",
+        tips: _ctx.translate("tips"),
+        open: _ctx.data.autoclose,
+        onClick: _cache[8] || (_cache[8] = ($event) => _ctx.open("autoclose")),
+        onClose: _cache[9] || (_cache[9] = ($event) => _ctx.close("autoclose"))
+      }, [
+        createBaseVNode("div", null, toDisplayString(_ctx.translate("topPosition")), 1)
+      ], 40, _hoisted_11)
+    ]),
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("customStyle:")), 1),
+    createBaseVNode("div", _hoisted_12, [
+      createBaseVNode("quark-tooltip", {
+        placement: "top",
+        autoclose: "",
+        closeable: "",
+        opentime: "5000",
+        scroolhidden: "",
+        tips: _ctx.translate("tips"),
+        open: _ctx.data.customstyle,
+        onClick: _cache[10] || (_cache[10] = ($event) => _ctx.open("customstyle")),
+        onClose: _cache[11] || (_cache[11] = ($event) => _ctx.close("customstyle")),
+        class: "custom-tips"
+      }, [
+        createBaseVNode("div", null, toDisplayString(_ctx.translate("topPosition")), 1)
+      ], 40, _hoisted_13)
+    ])
+  ]);
+}
+var demo = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { demo as default };
---
+++ demo/assets/demo.9d9b4aa0.js
@@ -0,0 +1,178 @@
+import { c as createComponent } from "./component.a18d28e2.js";
+import { _ as _export_sfc, o as onBeforeMount, u as useTranslate, c as createElementBlock, a as createBaseVNode, t as toDisplayString, b as openBlock } from "./mobile.6eaf977a.js";
+var demo_scss_vue_type_style_index_0_src_lang = "";
+const { createDemo, translate } = createComponent("grid");
+const _sfc_main = createDemo({
+  setup() {
+    onBeforeMount(() => {
+      useTranslate({
+        "zh-CN": {
+          basic: "\u57FA\u672C\u4F7F\u7528",
+          column: "\u81EA\u5B9A\u4E49\u5217\u6570",
+          square: "\u6B63\u65B9\u5F62\u683C\u5B50",
+          border: "\u65E0\u8FB9\u6846",
+          custom: "\u81EA\u5B9A\u4E49\u5185\u5BB9",
+          text: "\u6587\u5B57"
+        },
+        "en-US": {
+          basic: "Basic Usage",
+          column: "Custom Column",
+          square: "Square Cell",
+          border: "No Border",
+          custom: "Custom Content",
+          text: "text"
+        }
+      });
+    });
+    return {
+      translate
+    };
+  }
+});
+const _hoisted_1 = { class: "demo demo-badge" };
+const _hoisted_2 = ["text"];
+const _hoisted_3 = ["text"];
+const _hoisted_4 = ["text"];
+const _hoisted_5 = ["text"];
+const _hoisted_6 = { column: "3" };
+const _hoisted_7 = ["text"];
+const _hoisted_8 = ["text"];
+const _hoisted_9 = ["text"];
+const _hoisted_10 = ["text"];
+const _hoisted_11 = ["text"];
+const _hoisted_12 = ["text"];
+const _hoisted_13 = {
+  column: "3",
+  square: ""
+};
+const _hoisted_14 = ["text"];
+const _hoisted_15 = ["text"];
+const _hoisted_16 = ["text"];
+const _hoisted_17 = ["text"];
+const _hoisted_18 = ["text"];
+const _hoisted_19 = ["text"];
+const _hoisted_20 = { noborder: "" };
+const _hoisted_21 = ["text"];
+const _hoisted_22 = ["text"];
+const _hoisted_23 = ["text"];
+const _hoisted_24 = ["text"];
+const _hoisted_25 = /* @__PURE__ */ createBaseVNode("quark-grid", { column: "3" }, [
+  /* @__PURE__ */ createBaseVNode("quark-grid-item", null, [
+    /* @__PURE__ */ createBaseVNode("img", {
+      src: "https://m.hellobike.com/resource/helloyun/16682/dyElZ_img.png",
+      style: { "width": "40px" }
+    })
+  ]),
+  /* @__PURE__ */ createBaseVNode("quark-grid-item", null, [
+    /* @__PURE__ */ createBaseVNode("img", {
+      src: "https://m.hellobike.com/resource/helloyun/16682/dyElZ_img.png",
+      style: { "width": "40px" }
+    })
+  ]),
+  /* @__PURE__ */ createBaseVNode("quark-grid-item", null, [
+    /* @__PURE__ */ createBaseVNode("img", {
+      src: "https://m.hellobike.com/resource/helloyun/16682/dyElZ_img.png",
+      style: { "width": "40px" }
+    })
+  ])
+], -1);
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", _hoisted_1, [
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("basic")), 1),
+    createBaseVNode("quark-grid", null, [
+      createBaseVNode("quark-grid-item", {
+        icon: "https://m.hellobike.com/resource/helloyun/16682/dyElZ_img.png",
+        text: _ctx.translate("text")
+      }, null, 8, _hoisted_2),
+      createBaseVNode("quark-grid-item", {
+        icon: "https://m.hellobike.com/resource/helloyun/16682/dyElZ_img.png",
+        text: _ctx.translate("text")
+      }, null, 8, _hoisted_3),
+      createBaseVNode("quark-grid-item", {
+        icon: "https://m.hellobike.com/resource/helloyun/16682/dyElZ_img.png",
+        text: _ctx.translate("text")
+      }, null, 8, _hoisted_4),
+      createBaseVNode("quark-grid-item", {
+        icon: "https://m.hellobike.com/resource/helloyun/16682/dyElZ_img.png",
+        text: _ctx.translate("text")
+      }, null, 8, _hoisted_5)
+    ]),
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("column")), 1),
+    createBaseVNode("quark-grid", _hoisted_6, [
+      createBaseVNode("quark-grid-item", {
+        icon: "https://m.hellobike.com/resource/helloyun/16682/dyElZ_img.png",
+        text: _ctx.translate("text")
+      }, null, 8, _hoisted_7),
+      createBaseVNode("quark-grid-item", {
+        icon: "https://m.hellobike.com/resource/helloyun/16682/dyElZ_img.png",
+        text: _ctx.translate("text")
+      }, null, 8, _hoisted_8),
+      createBaseVNode("quark-grid-item", {
+        icon: "https://m.hellobike.com/resource/helloyun/16682/dyElZ_img.png",
+        text: _ctx.translate("text")
+      }, null, 8, _hoisted_9),
+      createBaseVNode("quark-grid-item", {
+        icon: "https://m.hellobike.com/resource/helloyun/16682/dyElZ_img.png",
+        text: _ctx.translate("text")
+      }, null, 8, _hoisted_10),
+      createBaseVNode("quark-grid-item", {
+        icon: "https://m.hellobike.com/resource/helloyun/16682/dyElZ_img.png",
+        text: _ctx.translate("text")
+      }, null, 8, _hoisted_11),
+      createBaseVNode("quark-grid-item", {
+        icon: "https://m.hellobike.com/resource/helloyun/16682/dyElZ_img.png",
+        text: _ctx.translate("text")
+      }, null, 8, _hoisted_12)
+    ]),
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("square")), 1),
+    createBaseVNode("quark-grid", _hoisted_13, [
+      createBaseVNode("quark-grid-item", {
+        icon: "https://m.hellobike.com/resource/helloyun/16682/dyElZ_img.png",
+        text: _ctx.translate("text")
+      }, null, 8, _hoisted_14),
+      createBaseVNode("quark-grid-item", {
+        icon: "https://m.hellobike.com/resource/helloyun/16682/dyElZ_img.png",
+        text: _ctx.translate("text")
+      }, null, 8, _hoisted_15),
+      createBaseVNode("quark-grid-item", {
+        icon: "https://m.hellobike.com/resource/helloyun/16682/dyElZ_img.png",
+        text: _ctx.translate("text")
+      }, null, 8, _hoisted_16),
+      createBaseVNode("quark-grid-item", {
+        icon: "https://m.hellobike.com/resource/helloyun/16682/dyElZ_img.png",
+        text: _ctx.translate("text")
+      }, null, 8, _hoisted_17),
+      createBaseVNode("quark-grid-item", {
+        icon: "https://m.hellobike.com/resource/helloyun/16682/dyElZ_img.png",
+        text: _ctx.translate("text")
+      }, null, 8, _hoisted_18),
+      createBaseVNode("quark-grid-item", {
+        icon: "https://m.hellobike.com/resource/helloyun/16682/dyElZ_img.png",
+        text: _ctx.translate("text")
+      }, null, 8, _hoisted_19)
+    ]),
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("border")), 1),
+    createBaseVNode("quark-grid", _hoisted_20, [
+      createBaseVNode("quark-grid-item", {
+        icon: "https://m.hellobike.com/resource/helloyun/16682/dyElZ_img.png",
+        text: _ctx.translate("text")
+      }, null, 8, _hoisted_21),
+      createBaseVNode("quark-grid-item", {
+        icon: "https://m.hellobike.com/resource/helloyun/16682/dyElZ_img.png",
+        text: _ctx.translate("text")
+      }, null, 8, _hoisted_22),
+      createBaseVNode("quark-grid-item", {
+        icon: "https://m.hellobike.com/resource/helloyun/16682/dyElZ_img.png",
+        text: _ctx.translate("text")
+      }, null, 8, _hoisted_23),
+      createBaseVNode("quark-grid-item", {
+        icon: "https://m.hellobike.com/resource/helloyun/16682/dyElZ_img.png",
+        text: _ctx.translate("text")
+      }, null, 8, _hoisted_24)
+    ]),
+    createBaseVNode("h2", null, toDisplayString(_ctx.translate("custom")), 1),
+    _hoisted_25
+  ]);
+}
+var demo = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { demo as default };
