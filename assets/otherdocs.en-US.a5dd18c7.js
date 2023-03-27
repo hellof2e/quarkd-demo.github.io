3b87b5d5afa02f5e7f8435b81e06923f254f845b
xxxsf <allan91coder@gmail.com>
2023-03-27 05:24:45 +0000

    Deploying to main from @ hellof2e/quark-design-docs@05a3597e0a1f8096c5e3e9e13e7cac0f9e1c59ab 🚀

---
+++ assets/otherdocs.en-US.a5dd18c7.js
@@ -0,0 +1,1 @@
+import{d as t,c as o,o as n}from"./vue.a54abc8b.js";const r={class:"markdown-body"},d={},m="",p=t({__name:"otherdocs.en-US",setup(c,{expose:e}){return e({frontmatter:{},excerpt:void 0}),(s,a)=>(n(),o("div",r))}});export{p as default,m as excerpt,d as frontmatter};
