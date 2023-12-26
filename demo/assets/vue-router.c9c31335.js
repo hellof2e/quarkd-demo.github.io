347ec06fd5f8ad815ed9ad4c94f3744a8b8cc4b1
sanqi-med <sanqi_med@163.com>
2023-12-26 09:02:59 +0000

    Deploying to main from @ hellof2e/quark-design@1b375647a6faa3cdf8dd87801d9ff177e24e4f75 🚀

---
+++ demo/assets/vue-router.c9c31335.js
@@ -0,0 +1,46 @@
+import { i as inject, j as onUnmounted, k as onDeactivated, l as onActivated } from "./mobile.50dfbfab.js";
+/*!
+  * vue-router v4.2.2
+  * (c) 2023 Eduardo San Martin Morote
+  * @license MIT
+  */
+var NavigationType;
+(function(NavigationType2) {
+  NavigationType2["pop"] = "pop";
+  NavigationType2["push"] = "push";
+})(NavigationType || (NavigationType = {}));
+var NavigationDirection;
+(function(NavigationDirection2) {
+  NavigationDirection2["back"] = "back";
+  NavigationDirection2["forward"] = "forward";
+  NavigationDirection2["unknown"] = "";
+})(NavigationDirection || (NavigationDirection = {}));
+var NavigationFailureType;
+(function(NavigationFailureType2) {
+  NavigationFailureType2[NavigationFailureType2["aborted"] = 4] = "aborted";
+  NavigationFailureType2[NavigationFailureType2["cancelled"] = 8] = "cancelled";
+  NavigationFailureType2[NavigationFailureType2["duplicated"] = 16] = "duplicated";
+})(NavigationFailureType || (NavigationFailureType = {}));
+const matchedRouteKey = Symbol("");
+function registerGuard(record, name, guard) {
+  const removeFromList = () => {
+    record[name].delete(guard);
+  };
+  onUnmounted(removeFromList);
+  onDeactivated(removeFromList);
+  onActivated(() => {
+    record[name].add(guard);
+  });
+  record[name].add(guard);
+}
+function onBeforeRouteLeave(leaveGuard) {
+  const activeRecord = inject(
+    matchedRouteKey,
+    {}
+  ).value;
+  if (!activeRecord) {
+    return;
+  }
+  registerGuard(activeRecord, "leaveGuards", leaveGuard);
+}
+export { onBeforeRouteLeave as o };
